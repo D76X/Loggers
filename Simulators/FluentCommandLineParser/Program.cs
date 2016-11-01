0053ed724b552aa8458b7147fa75ef5715278a5d
C78X <cinzia.nava.x@gmail.com>
2016-11-01 11:10:13 +0000

    refactored fluent command line parser

--- Simulators/FluentCommandLineParser/Program.cs
+++ Simulators/FluentCommandLineParser/Program.cs
@@ -1,44 +1,45 @@
-﻿using System;
+﻿using Fclp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Fclp;
-
-namespace FluentCommandLine
+namespace CommandLineFluent
 {
     class Program
     {
-        public class AppArguments
-        {
-            public int RecordId { get; set; }
-            public bool Silent { get; set; }
-            public string NewValue { get; set; }
-        }
         static void Main(string[] args)
         {
+            var parser = new FluentCommandLineParser<Arguments>();
+            parser
+                .Setup(a => a.Address)
+                .As('a', "address")
+                .WithDescription("The URL to connect to the service.")
+        .Required();
 
-            var p = new FluentCommandLineParser<AppArguments>();
+            parser
+                .Setup(a => a.Username)
+                .As('u', "username")
+                .WithDescription("The username to use for authentication.");
 
-            p.Setup(arg => arg.RecordId)
-             .As('r', "record") // define the short and long option name
-             .Required(); // using the standard fluent Api to declare this Option as required.
+            parser
+                .Setup(a => a.Password)
+                .As('p', "password")
+                .WithDescription("The password to use for authentication.");
 
-            p.Setup(arg => arg.NewValue)
-             .As('v', "value")
-             .Required();
+            var parsedArgs = parser.Parse(args);
 
-            p.Setup(arg => arg.Silent)
-             .As('s', "silent")
-             .SetDefault(false);
-            var result = p.Parse(args);
+            Console.WriteLine("You entered  {0}  ", parser.Object.Password);
+            ////Console.WriteLine("You entered  {0}  ",  parser.Object.Username);
+            //Console.WriteLine("You entered  {0}  ", parser.Object.Address);
+        }
 
-            if (result.HasErrors == false)
-            {
-                // use the instantiated ApplicationArguments object from the Object property on the parser.
-                application.Run(p.Object);
-            }
+        private class Arguments
+        {
+            public string Address { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
         }
     }
 }
