7a95d15cec49c5a7ed82135307f4cfa1968447e4
C78X <cinzia.nava.x@gmail.com>
2016-11-04 15:00:43 +0000

    Refactored FluentCommandLine

--- Simulators/CommandLineFluent/Program.cs
+++ Simulators/CommandLineFluent/Program.cs
@@ -17,37 +17,47 @@ namespace CommandLineFluent
         }
         static void Main(string[] args)
         {
-            var parser = new FluentCommandLineParser ();
+            //var parser = new FluentCommandLineParser ();
 
-            parser.Setup<string>('a', "address")
-                  .Required();
+            //parser.Setup<string>('a', "address")
+            //      .Required()
+            //      .Callback(text => Console.WriteLine(text));
 
-            parser.Setup<string>('u', "username")
-                 .Required();
+            //parser.Setup<string>('u', "username")
+            //     .Required()
+            //     .Callback(text => Console.WriteLine(text));
 
-            parser.Setup<string>('p', "password")
-                 .Required();
+            //parser.Setup<string>('p', "password")
+            //     .Required()
+            //     .Callback(text => Console.WriteLine(text));
 
-            var parsedValues = parser.Parse(args);
+            //parser.Parse(args);
 
-           
-            //    parser
-            //        .Setup(a => a.Address)
-            //        .As('a', "address")
-            //        .WithDescription("The URL to connect to the service.")
-            //.Required();
 
-            //    parser
-            //        .Setup(a => a.Username)
-            //        .As('u', "username")
-            //        .WithDescription("The username to use for authentication.");
+            //GENERIC FLUENT CLP
+            var parser2 = new FluentCommandLineParser<Arguments>();
+            parser2
+                .Setup(a => a.Address)
+                .As('a', "address")
+                .WithDescription("The URL to connect to the service.")
+                .Required();            
 
-            //    parser
-            //        .Setup(a => a.Password)
-            //        .As('p', "password")
-            //        .WithDescription("The password to use for authentication.");
+            parser2
+                .Setup(a => a.Username)
+                .As('u', "username")
+                .WithDescription("The username to use for authentication.");
 
-          
+            parser2
+                .Setup(a => a.Password)
+                .As('p', "password")
+                .WithDescription("The password to use for authentication.")
+                .Callback(text => Console.WriteLine(text));
+
+              parser2.Parse(args);
+
+             
+
+
         }
     }
 }
