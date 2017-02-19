d7b9a15c2526b96fb83941d8a4b933742297540c
Davide Spano <davide.spano.x@gmail.com>
2017-02-19 11:22:15 +0000

    added WpfEmf.ViewModels project and ProductViewModel class

---
+++ Loggers/Composition-Examples/Advanced-1/WpfEmf.ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfEmf.Interfaces;
+
+namespace WpfEmf.ViewModels
+{
+    public class ProductViewModel: WorkSpaceViewModel {
+
+        public ProductViewModel() {
+            this.HeaderText = "ProductViewModel";
+        }
+    }
+}
