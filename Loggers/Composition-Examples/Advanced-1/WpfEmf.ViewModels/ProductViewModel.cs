438df008d196fa563fdaf3d80fe62cc2c974a703
Davide Spano <davide.spano.x@gmail.com>
2017-03-05 13:15:39 +0000

    changed ProductViewModel with ProductCounter property changed MainWindow.xaml, ProductUserControl.xaml, ProductResourceDictionary.xaml with new text box for Product Counter and other minor changes in the layout.

--- Loggers/Composition-Examples/Advanced-1/WpfEmf.ViewModels/ProductViewModel.cs
+++ Loggers/Composition-Examples/Advanced-1/WpfEmf.ViewModels/ProductViewModel.cs
@@ -3,10 +3,17 @@ using WpfEmf.Interfaces;
 
 namespace WpfEmf.ViewModels {
     [Export(typeof(ProductViewModel))]
-    public class ProductViewModel: WorkSpaceViewModel {
+    public class ProductViewModel: WorkSpaceViewModel {        
 
         public ProductViewModel() {
             this.HeaderText = "ProductViewModel";
+        }
+
+        private int productCounter;
+
+        public int ProductCounter {
+            get { return productCounter; }
+            set { SetProperty<int>(ref productCounter,value,nameof(ProductCounter)); }
         }
     }
 }
