1c0e55444a6a732ff9525a52623d570b885247e5
Davide Spano <davide.spano.x@gmail.com>
2017-03-11 12:16:04 +0000

    changed PluginCustomer - added basic code to load the resource dictionary in the cosntructor and reference to the ViewModel project

--- Loggers/Composition-Examples/Advanced-1/WpfEmf.Plugin.Customer.WpfControlLibrary/PluginCustomer.cs
+++ Loggers/Composition-Examples/Advanced-1/WpfEmf.Plugin.Customer.WpfControlLibrary/PluginCustomer.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Composition;
 using System.Windows;
 using WpfEmf.Interfaces;
+using WpfEmf.ViewModels;
 
 /// <summary>
 /// http://stackoverflow.com/questions/12204614/load-wpf-ui-into-mvvm-application-from-plug-in-assembly
@@ -11,19 +12,34 @@ namespace WpfEmf.Plugin.Customer.WpfControlLibrary {
     [Export(typeof(IBasePlugin))]
     public class PluginCustomer : IBasePlugin {
 
+        private CustomerViewModel _viewModel { get; set; }
+
+        private ResourceDictionary _viewDictionary = new ResourceDictionary();
+
+
+        /// <summary>
+        /// Use importing constructor if you want to import MEF dependencies
+        /// at construction time as explaied here http://www.brendanforster.com/mef-import-vs-importingconstructor.html
+        /// </summary>
         public PluginCustomer() {
 
+            // there exist two URI styles - the first is an example of PACK URI  
+
+            ResourceDictionary testResourceDisctionary = new ResourceDictionary();
+            testResourceDisctionary.Source = new Uri("pack://application:,,,/WpfEmf.Plugin.Customer.WpfControlLibrary;component/CustomerResourceDictionary.xaml", UriKind.RelativeOrAbsolute);
+
+            this._viewDictionary.Source = new Uri("/WpfEmf.Plugin.Customer.WpfControlLibrary;component/CustomerResourceDictionary.xaml", UriKind.RelativeOrAbsolute);
         }
 
         public ResourceDictionary View {
             get {
-                throw new NotImplementedException();
+                return this._viewDictionary;
             }
         }
 
         public WorkSpaceViewModel ViewModel {
             get {
-                throw new NotImplementedException();
+                return this._viewModel == null ? this._viewModel = new CustomerViewModel() : this._viewModel;
             }
         }
     }
