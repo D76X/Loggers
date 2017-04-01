f3fd75b35a1e09fbeacfa8b332087b4d699506db
Davide Spano <davide.spano.x@gmail.com>
2017-04-01 13:08:55 +0100

    added HelloWix-3 project with basic wxs and binary assets HelloWixTestLib1 and HelloWixTestApp1

---
+++ Examples/Installers-Examples/HelloWixTestLib1/HelloWixClass1.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWixTestLib1 {
+    public class HelloWixClass1 {
+        public HelloWixClass1() {
+            Message = nameof(HelloWixClass1);
+        }
+
+        public HelloWixClass1(string message) {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+}
--- Examples/Installers-Examples/HelloWixTetsApp1/Program.cs
+++ Examples/Installers-Examples/HelloWixTetsApp1/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelloWixTestLib1;
+using System;
 
 namespace HelloWixTetsApp1 {
     class Program {
         static void Main(string[] args) {
+            var program = new Program();
+            program.Run();
+            Console.WriteLine(nameof(HelloWixTetsApp1)+" completed");
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+
+        private void Run() {
+
+            var greeater1 = new HelloWixClass1();
+            var greeater2 = new HelloWixClass1("Have a good day!");
+            Console.WriteLine(greeater1.Message);
+            Console.WriteLine(greeater2.Message);
         }
     }
 }
