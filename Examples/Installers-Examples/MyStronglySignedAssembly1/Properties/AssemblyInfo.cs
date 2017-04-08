20c35f6e463ce997a5f5ec072126d0493ae55689
Davide Spano <davide.spano.x@gmail.com>
2017-04-08 17:19:09 +0100

    changed assembly info for MyStronglySignedAssembly1.dll

--- Examples/Installers-Examples/MyStronglySignedAssembly1/Properties/AssemblyInfo.cs
+++ Examples/Installers-Examples/MyStronglySignedAssembly1/Properties/AssemblyInfo.cs
@@ -6,13 +6,15 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyStronglySignedAssembly1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Test a strongly signed assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("New Thining Technologies")]
 [assembly: AssemblyProduct("MyStronglySignedAssembly1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyKeyFile("MyStronglySignedAssembly1Key.snk")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -34,3 +36,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
