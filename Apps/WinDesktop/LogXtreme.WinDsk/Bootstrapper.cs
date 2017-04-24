cd9e94c9a3b7e3628163b7e1bc94c8c8c80e8013
Davide Spano <davide.spano.x@gmail.com>
2017-04-24 17:50:32 +0100

    added notes and basic structure for LogXtreme to start a simple Shell

---
+++ Apps/WinDesktop/LogXtreme.WinDsk/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using System.Windows;
+
+namespace LogXtreme.WinDsk {
+    public class Bootstrapper: UnityBootstrapper {
+
+        protected override DependencyObject CreateShell() {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell() {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window)Shell;                
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ Apps/WinDesktop/LogXtreme.WinDsk/Shell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LogXtreme.WinDsk {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : Window {
+        public Shell() {
+            InitializeComponent();
+        }
+    }
+}
