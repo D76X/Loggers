f29c098c77b05319afc8ceff14174e7b06869269
Davide Spano <davide.spano.x@gmail.com>
2017-04-24 22:10:30 +0100

    extensions of the basics of LogXtreme

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -0,0 +1,8 @@
+﻿
+namespace LogXtreme.WinDsk.Infrastructure {    
+
+    public static class RegionNames {
+        public const string RegionContent = "RegionContent";
+        public const string RegionToolbar = "RegionToolbar";
+    }
+}
