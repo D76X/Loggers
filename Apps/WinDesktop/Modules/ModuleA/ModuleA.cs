d3d4cbf40479cb7ab91686a0170bf671c799d4ee
Davide Spano <davide.spano.x@gmail.com>
2017-04-24 20:57:34 +0100

    added ModuleA to WinDesktop

---
+++ Apps/WinDesktop/Modules/ModuleA/ModuleA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Prism.Modularity;
+
+namespace ModuleA {
+    public class ModuleA : IModule {
+
+         
+        public void Initialize() {
+            throw new NotImplementedException();
+        }
+    }
+}
