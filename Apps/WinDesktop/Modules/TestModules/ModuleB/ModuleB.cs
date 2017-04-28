881596dad4b44ebf36721e80485f50adabf69332
Davide Spano <davide.spano.x@gmail.com>
2017-04-28 21:43:24 +0100

    notes and the basics of scoped regions

--- Apps/WinDesktop/Modules/TestModules/ModuleB/ModuleB.cs
+++ Apps/WinDesktop/Modules/TestModules/ModuleB/ModuleB.cs
@@ -31,23 +31,37 @@ namespace ModuleB {
             // However ViewA also holds a child view ViewB within itself 
             // The child view ViweB is injected into the local region RegionChild
 
-            // a region name becomes a key for a region type in the instance of the RegionManager
+            // a region name is a key for a region type in an instance of the RegionManager
+            // in this case a RegionChild becomes a key for the default global region manager 
             this.regionManager.RegisterViewWithRegion(RegionNamesModuleB.RegionChild, typeof(ViewB));
 
-            IRegion contentRegion = this.regionManager.Regions[RegionNames.RegionContent];
 
-            // resolve the view and inject it into its region then activate it 
+            // IRegion targetRegion = this.regionManager.Regions[RegionNames.RegionContent];
+            IRegion targetRegion = this.regionManager.Regions[RegionNames.RegionToolbar];
+
+            string viewName = null;
+            bool createRegionManagerScope = true;
+
+            // resolve the view type ViewA and inject it into the content region then activate it
+            // the ViewA host a child view of type ViewB which is resitered as a region on the 
+            // global region manager.  
             var view1 = this.container.Resolve<ViewA>();
-            contentRegion.Add(view1);
-            contentRegion.Activate(view1);
+            targetRegion.Add(view1, viewName, createRegionManagerScope);
+            targetRegion.Activate(view1);
 
-            // Cannot add the same view twice because any region name must be unique within a RegionManager instance
-            // by adding an instance of the same view type to the same region manager the key is duplicated and exc
-            // is thrown 
+            // Despite the fact that the host control for the content region is a StackPanel a second ViewA 
+            // cannot be added to the content region because ViewA holds a ViewB instance as its child and 
+            // ViewB is registered on the global region manager with the key "RegionChild". Therefore when 
+            // it is attempted to add a second instance of ViewA this also causes the global region manager
+            // to try to add a second "RegionChild" key which is not allowed and an exception is thrown.  
 
             // var view2 = this.container.Resolve<ViewA>();
             // contentRegion.Add(view2);
             // contentRegion.Activate(view2);
+
+            var view2 = this.container.Resolve<ViewA>();
+            targetRegion.Add(view2, viewName, createRegionManagerScope);
+            targetRegion.Activate(view2);
         }
     }
 }
