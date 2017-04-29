ce3522966e9b532be2ac5245843be90fb653cc55
Davide Spano <davide.spano.x@gmail.com>
2017-04-29 13:19:24 +0100

    setting up new ModuleC

---
+++ Apps/WinDesktop/Modules/TestModules/ModuleC/ModuleC.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Prism.Modularity;
+
+namespace ModuleC {
+    public class ModuleC : IModule {
+        public void Initialize() {
+            throw new NotImplementedException();
+        }
+    }
+}
