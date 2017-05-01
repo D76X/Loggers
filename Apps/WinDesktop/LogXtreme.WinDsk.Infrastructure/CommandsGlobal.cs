ad563b130386e049aa938a1dd2f809cb0a737377
Davide Spano <davide.spano.x@gmail.com>
2017-05-01 11:52:34 +0100

    added CommandsGlobal.cs

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/CommandsGlobal.cs
@@ -0,0 +1,8 @@
+﻿using Prism.Commands;
+
+namespace LogXtreme.WinDsk.Infrastructure {
+    public static class CommandsGlobal {
+
+        public static CompositeCommand SaveSession = new CompositeCommand();
+    }
+}
