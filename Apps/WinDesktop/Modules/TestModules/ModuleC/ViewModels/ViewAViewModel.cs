3f776965e880bc8651cbff5a7c9972a6e4f9dfd6
Davide Spano <davide.spano.x@gmail.com>
2017-05-06 11:05:24 +0100

    changes ModuleC's views and view models to have navigation

--- Apps/WinDesktop/Modules/TestModules/ModuleC/ViewModels/ViewAViewModel.cs
+++ Apps/WinDesktop/Modules/TestModules/ModuleC/ViewModels/ViewAViewModel.cs
@@ -1,11 +1,27 @@
-﻿using LogXtreme.WinDsk.Infrastructure;
+﻿using System;
+using LogXtreme.WinDsk.Infrastructure;
 using ModuleC.Interfaces;
+using Prism.Commands;
+using Prism.Regions;
 
 namespace ModuleC.ViewModels {
-    public class ViewAViewModel : IViewAViewModel {
+    public class ViewAViewModel : 
+        ViewModelBase,
+        IViewAViewModel {
 
-        public ViewAViewModel() {
+        private readonly IRegionManager regionManager;
 
-        }       
+        public DelegateCommand NavigateCommand { get; private set; }
+
+        public ViewAViewModel(IRegionManager regionManager) {
+
+            this.regionManager = regionManager;
+
+            this.NavigateCommand = new DelegateCommand(Navigate);
+        }
+
+        private void Navigate() {
+            this.regionManager.RequestNavigate(RegionNames.RegionContent, "ViewB");
+        }
     }
 }
--- Apps/WinDesktop/Modules/TestModules/ModuleC/ViewModels/ViewBViewModel.cs
+++ Apps/WinDesktop/Modules/TestModules/ModuleC/ViewModels/ViewBViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using LogXtreme.WinDsk.Infrastructure;
 using ModuleC.Interfaces;
+using Prism.Commands;
+using Prism.Regions;
 
 namespace ModuleC.ViewModels {
-    public class ViewBViewModel : IViewBViewModel {
-        public ViewBViewModel() {
+    public class ViewBViewModel : 
+        ViewModelBase,
+        IViewBViewModel {
 
+        private readonly IRegionManager regionManager;
+
+        public DelegateCommand NavigateCommand { get; private set; }
+
+        public ViewBViewModel(IRegionManager regionManager) {
+
+            this.regionManager = regionManager;
+
+            this.NavigateCommand = new DelegateCommand(Navigate);
+        }
+
+        private void Navigate() {
+            this.regionManager.RequestNavigate(RegionNames.RegionContent, "ViewA");
         }
     }
 }
