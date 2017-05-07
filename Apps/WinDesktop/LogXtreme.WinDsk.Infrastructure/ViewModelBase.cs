ec65c6efa092be16a57ce1aa259207e86c0d9ed6
Davide Spano <davide.spano.x@gmail.com>
2017-05-07 13:55:50 +0100

    ViewModelBase implements INavigationAware and declares its members as virtual

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ViewModelBase.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ViewModelBase.cs
@@ -1,6 +1,19 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using Prism.Mvvm;
+using Prism.Regions;
 
 namespace LogXtreme.WinDsk.Infrastructure {
-    public class ViewModelBase : BindableBase, IViewModelBase {
+
+    public class ViewModelBase : BindableBase, IViewModelBase, INavigationAware {
+
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext) {
+            return true;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext) {
+        }
+
+        public virtual void OnNavigatedTo(NavigationContext navigationContext) {
+        }
     }
 }
