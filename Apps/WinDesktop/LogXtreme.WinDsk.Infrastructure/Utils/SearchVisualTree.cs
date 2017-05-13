991a9b7861e71127fa47a3936edbda199a67e1f6
Davide Spano <davide.spano.x@gmail.com>
2017-05-13 13:53:37 +0100

    added SearchVisualTree utils

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Utils/SearchVisualTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace LogXtreme.WinDsk.Infrastructure.Utils {
+    static class SearchVisualTree {
+
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject {
+
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null) {
+                return null;
+            }
+
+            var parent = parentObject as T;
+
+            if (parent != null) {
+                return parent;
+            }
+
+            return FindParent<T>(parentObject);
+        }
+    }
+}
