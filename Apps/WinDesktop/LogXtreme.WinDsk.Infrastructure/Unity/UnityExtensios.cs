49cbec9888086387f08b806900444bc60ec9bbcb
Davide Spano <davide.spano.x@gmail.com>
2017-05-14 11:33:15 +0100

    added UnityExtensions. RegisterTypeForNavigation

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Unity/UnityExtensios.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace LogXtreme.WinDsk.Infrastructure.Unity {
+
+    public static class UnityExtensios {
+
+        /// <summary>
+        /// This extension method is to make it easier and more explicit to register views 
+        /// and view models types for navigation via Prism mavigation framework with the 
+        /// DI container. Notice that the navigatiuon name typeof(T).FullName is the fully
+        /// qualified name of the type.
+        /// </summary>
+        /// <typeparam name="T">The type of a view or view model to navigate to</typeparam>
+        /// <param name="container">The refence to THIS of teh extension method</param>
+        public static void RegisterTypeForNavigation<T>(this IUnityContainer container) {
+            container.RegisterType(typeof(object), typeof(T), typeof(T).FullName);
+        }
+    }
+}
