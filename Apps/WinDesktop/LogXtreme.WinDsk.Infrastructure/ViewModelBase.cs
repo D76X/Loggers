1ded6a1093c5674afa41e858c2737e39c71df01b
Davide Spano <davide.spano.x@gmail.com>
2017-05-14 14:27:26 +0100

    changed ViewModelBase replaced INavigationAware with IConfirmNavigationRequest and added summaries

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ViewModelBase.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ViewModelBase.cs
@@ -4,10 +4,16 @@ using Prism.Regions;
 
 namespace LogXtreme.WinDsk.Infrastructure {
 
+    /// <summary>
+    /// The Base View Model.
+    /// 
+    /// IConfirmNavigationRequest inherits from INavigationAware.
+    /// 
+    /// </summary>
     public class ViewModelBase : 
         BindableBase, 
         IViewModelBase, 
-        INavigationAware, 
+        IConfirmNavigationRequest, 
         IRegionMemberLifetime {
 
         /// <summary>
@@ -18,9 +24,32 @@ namespace LogXtreme.WinDsk.Infrastructure {
         /// and view model are dereferenced and left to the GC. When KeepAlive => true
         /// the view and view model are kept in memory.
         /// </summary>
-        public virtual bool KeepAlive => false;
+        public virtual bool KeepAlive => true;
+
+        /// <summary> 
+        /// IConfirmNavigationRequest inherits from INavigationAware.
+        /// 
+        /// When a Navigation Request is made to the RegionManager the view and then view 
+        /// model of the current active view are checked to determined whether they implement 
+        /// the interface IConfirmNavigationRequest. If this is the case the implementation 
+        /// of ConfirmNavigationRequest is invoked to confirm to the RegionManager whether 
+        /// the navigation should continue or not.
+        /// 
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        /// <param name="continuationCallback"></param>
+        public virtual void ConfirmNavigationRequest(
+            NavigationContext navigationContext, 
+            Action<bool> continuationCallback) {
+            // check NavigationContext...
+            // check state and data...
+            continuationCallback(true);
+        }
 
         /// <summary>
+        /// 
+        /// INavigationAware.IsNavigationTarget
+        /// 
         /// When a Navigation Request is made to the RegionManager the IsNavigationTarget
         /// of each view instance implementing INavigationAware that exists in the target 
         /// region will be checked. If there is a INavigationAware instance that returns 
@@ -32,11 +61,14 @@ namespace LogXtreme.WinDsk.Infrastructure {
         /// <param name="navigationContext"></param>
         /// <returns>TRUE to activate the view, FALSE otherwise</returns>
         public virtual bool IsNavigationTarget(NavigationContext navigationContext) {
-            // 
+            // check the NavigationContext
             return true;
         }
 
         /// <summary>
+        /// 
+        /// INavigationAware.OnNavigatedFrom
+        /// 
         /// When a Navigation Request is made to the RegionManager the OnNavigatedFrom
         /// occurs on the view model for the active view implementing INavigationAware. 
         /// In INavigationAware.OnNavigatedFrom state, data or teardown, etc. related 
@@ -50,6 +82,9 @@ namespace LogXtreme.WinDsk.Infrastructure {
         }
 
         /// <summary>
+        /// 
+        /// INavigationAware.OnNavigatedtO
+        /// 
         /// When a Navigation Request is made to the RegionManager and a view is the 
         /// selected target of the navigation OnNavigatedTo is invoked. OnNavigatedTo
         /// may be used to execute initialisation and/or use oarameter available on
