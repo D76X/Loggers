de5b3cceaf31bd9dd20e3273d0c055d47875cdc2
Davide Spano <davide.spano.x@gmail.com>
2017-05-27 10:55:51 +0100

    changes in 1Basic.fs

--- FSharp/Samples/1Basics.fs
+++ FSharp/Samples/1Basics.fs
@@ -5,13 +5,16 @@ let items = [1..5]
 let itemsPlusOne = List.append items [6]
 items
 
-let prefix prefixStr baseStr =
-    prefixStr+","+baseStr
+let prefix prefixStr baseStr = prefixStr+","+baseStr
 
 let myPrefix = prefix "Hello"
 
 let names = ["one"; "two"; "three"]
 
-let prefixedNames = names
+let bang stringVal = stringVal+"!" 
+
+let modifiedNames = names
                     |>Seq.map(myPrefix)
+                    |>Seq.map(bang)
+                    |>Seq.toList
 
