c7d1230555886779b1a37782428d8c0d66a665f2
Davide Spano <davide.spano.x@gmail.com>
2017-05-28 15:47:53 +0100

    added few region names ro RegionNames.cs

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -2,7 +2,13 @@
 namespace LogXtreme.WinDsk.Infrastructure {    
 
     public static class RegionNames {
+
         public const string RegionContent = "RegionContent";
+
+        public const string RegionMainMenu = "RegionMainMenu";
         public const string RegionToolbar = "RegionToolbar";
+        public const string RegionLeftPanels = "RegionLeftPanels";
+        public const string RegionRightPanels = "RegionRightPanels";
+        public const string RegionStatusBar = "RegionStatusBar";
     }
 }
