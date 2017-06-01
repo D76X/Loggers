49f737a74cd5c588b123acd3c1a7ddcd2b58f511
Davide Spano <davide.spano.x@gmail.com>
2017-06-01 13:11:33 +0100

    adde Module attribute with ModuleName for MainMenuModule

---
+++ Apps/WinDesktop/Modules/InfrastructureModules/MainMenuOptionsModule/MainMenuOptionsModule.cs
@@ -0,0 +1,32 @@
+﻿using LogXtreme.WinDsk.Infrastructure.Menu;
+using LogXtreme.WinDsk.Infrastructure.Services;
+using MainMenuModule.ViewModels;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+
+namespace MainMenuOptionsModule {
+
+    [ModuleDependency(nameof(MainMenuModule))]
+    public class MainMenuOptionsModule : IModule {
+
+        private readonly IMenuService menuService;
+
+        public MainMenuOptionsModule(IMenuService menuService) {
+            this.menuService = menuService;
+        }
+
+        public void Initialize() {
+
+            // Register types
+
+            // Subscribe to Services or Events
+
+            // Register Shared Services
+
+            // Compose Views into the Shell
+
+            //...
+            this.menuService.AddTopLevelMenu(new MenuItemNode() { Text = "Options"});
+        }
+    }
+}
