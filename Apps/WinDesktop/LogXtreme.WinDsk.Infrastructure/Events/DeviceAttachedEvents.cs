aba042b9d79db7def0f9ae4d0e110275c848a1d9
Davide Spano <davide.spano.x@gmail.com>
2017-06-02 13:15:09 +0100

    added DeviceAttachedEvents

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Events/DeviceAttachedEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+
+namespace LogXtreme.WinDsk.Infrastructure.Events {
+
+    public class Device { };
+    
+    public class DeviceAttachedEvent : PubSubEvent<Device> { };
+
+}
