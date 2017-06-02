cfd2f690973dced178d4fa534ae540da1e38063b
Davide Spano <davide.spano.x@gmail.com>
2017-06-02 11:00:19 +0100

    renamed IMenuItemNode to IMenuItem renamed MenuItemNode to MenuItemViewModel

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Menu/IMenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LogXtreme.WinDsk.Infrastructure.Menu {
+    public interface IMenuItem {
+
+        string Header { get; }
+
+        IMenuItem Parent { get; }
+
+        IEnumerable<IMenuItem> Children { get; }
+    }
+}
---
+++ Apps/WinDesktop/Modules/InfrastructureModules/MainMenuModule/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using LogXtreme.WinDsk.Infrastructure.Menu;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace MainMenuModule.ViewModels {
+
+    public class MenuItemViewModel : BindableBase, IMenuItem {
+
+        private string header;
+        private IMenuItem parent;
+        private IEnumerable<IMenuItem> children;
+
+        public MenuItemViewModel(
+            string header,
+            IEnumerable<IMenuItem> children) {
+
+            this.header = header;
+            this.children = children;
+        }
+
+        public string Header {
+            get { return this.header; }
+            set { this.SetProperty(ref this.header, value); }
+        }
+
+        public IMenuItem Parent {
+            get { return this.parent; }
+            set { this.SetProperty(ref this.parent, value); }
+        }
+
+        public IEnumerable<IMenuItem> Children => this.children;
+    }
+}
