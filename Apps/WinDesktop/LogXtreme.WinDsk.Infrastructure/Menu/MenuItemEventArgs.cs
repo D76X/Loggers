21256098523f81d5ba3e4bd8348b297bee472727
Davide Spano <davide.spano.x@gmail.com>
2017-06-02 16:55:00 +0100

    added MenuItemEventArgs

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Menu/MenuItemEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogXtreme.WinDsk.Infrastructure.Menu {
+    public class MenuItemEventArgs : EventArgs {
+
+        public readonly IMenuItem MenuItem;
+
+        public MenuItemEventArgs(IMenuItem menuItem) {
+            this.MenuItem = menuItem;
+        }
+    }
+}
