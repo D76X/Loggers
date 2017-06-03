7d146f5780bb8f5d7b4a1b669c0849aa3e64e218
Davide Spano <davide.spano.x@gmail.com>
2017-06-03 13:36:30 +0100

    added NainMenuHeadres.cs to MainMenuModule project

---
+++ Apps/WinDesktop/Modules/InfrastructureModules/MainMenuModule/MainMenuHeaders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMenuModule {
+    static class MainMenuHeaders {
+        public const string File = @"_File";
+        public const string Edit = @"_Edit";
+        public const string View = @"_View";
+    }
+}
