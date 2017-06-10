e799276fbd816828159e5546483f98bdcd540396
Davide Spano <davide.spano.x@gmail.com>
2017-06-10 17:17:21 +0100

    added some implementation of FromWeakEvent and WeakEventForwarded

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ReactiveExtensions/WeakEventForwarded.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LogXtreme.WinDsk.Infrastructure.ReactiveExtensions {
+
+    internal class WeakEventForwarded<TEventArgs, TEventSource> : IDisposable {
+
+        private WeakEventHandler weakEventHanlder;
+        private IObserver<TEventArgs> observer;
+
+        public WeakEventForwarded(
+            TEventSource eventSource,
+            Action<TEventSource, EventHandler> subscribeHandler,
+            Action<TEventSource, EventHandler> unsubscribeHandler,
+            IObserver<EventArgs> observer) {
+
+            
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
