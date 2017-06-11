482339d9643851940172d129a44a1f5b6c43a247
Davide Spano <davide.spano.x@gmail.com>
2017-06-11 13:04:26 +0100

    added Instrumentation/SemanticLogging project

---
+++ Apps/Instrumentation/SemanticLogging/SimpleEventSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace SemanticLogging {
+
+    [EventSource(Name = "NewThinkingTechnologies-LogXtreme-SimpleEventSource")]
+    public sealed class SimpleEventSource : EventSource {
+
+        public static SimpleEventSource Log = new SimpleEventSource();
+        
+        public void Message(string message) {
+
+            WriteEvent(1, message);
+        }
+
+        public void AccessByPrimaryKey(string primaryKey, string tableName) {
+
+            WriteEvent(2, primaryKey, tableName);
+        }
+    }
+}
