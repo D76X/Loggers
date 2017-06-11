7f115ca50bf6c60014ce6bfb1f6a84f3cb4341b3
Davide Spano <davide.spano.x@gmail.com>
2017-06-11 15:49:07 +0100

    added more implementation for WeakEventForwarder

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ReactiveExtensions/WeakEventForwarder.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/ReactiveExtensions/WeakEventForwarder.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using LogXtreme.WinDsk.Infrastructure.Events;
+using System;
 
 namespace LogXtreme.WinDsk.Infrastructure.ReactiveExtensions {
 
-    internal class WeakEventForwarder<TEventArgs, TEventSource> : IDisposable {
+    internal class WeakEventForwarder<TEventArgs, TEventSource> : IDisposable 
+        where  TEventArgs : EventArgs 
+        where TEventSource : class {
 
-        //private WeakEventHandler weakEventHanlder;
+        private WeakEventHanlder weakEventHanlder;
         private IObserver<TEventArgs> observer;
 
         public WeakEventForwarder(
@@ -13,7 +16,26 @@ namespace LogXtreme.WinDsk.Infrastructure.ReactiveExtensions {
             Action<TEventSource, EventHandler> unsubscribeHandler,
             IObserver<EventArgs> observer) {
 
-            
+            this.observer = (IObserver<TEventArgs>)observer;
+
+            var listeningObject = this;
+
+            // get a weak reference to a listener object
+            // the listener is this object
+            // do not pass *this* in the last delegate because
+            // it would crate a strong reference to this object
+            // from the delegate by closure, pass *me* instead
+            // so that a static method 
+            this.weakEventHanlder = WeakEventHanlder.Register(
+                eventSource,
+                subscribeHandler,
+                unsubscribeHandler,
+                listeningObject,
+                (me, sender, args) => me.OnNext((TEventArgs)args));
+        }
+
+        private void OnNext(TEventArgs args) {
+            this.observer.OnNext(args);
         }
 
         public void Dispose() {
