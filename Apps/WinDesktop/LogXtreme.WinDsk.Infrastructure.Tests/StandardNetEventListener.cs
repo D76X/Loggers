fd0ef2a5a965baf013e2cf7af0b2ba1069e13bd6
Davide Spano <davide.spano.x@gmail.com>
2017-06-18 14:47:45 +0100

    StandardNetEvent Listener and Source takes finalization traker

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/StandardNetEventListener.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/StandardNetEventListener.cs
@@ -5,12 +5,21 @@ namespace LogXtreme.WinDsk.Infrastructure.Tests {
     internal class StandardNetEventListener {
 
         private int invokations = 0;
+        private FinalizeTracker finalizeTracker;
+
+        public StandardNetEventListener(ref FinalizeTracker finalizeTracker) {
+            this.finalizeTracker = finalizeTracker;
+        }
 
         public void OnEvent(object source, EventArgs args) {
             this.invokations += 1;
         }
 
         public int Invokations => this.invokations;
+
+        ~StandardNetEventListener() {
+            this.finalizeTracker.MarkAsFinalized();
+        }
     }
 }
 
--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/StandardNetEventSource.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/StandardNetEventSource.cs
@@ -3,10 +3,21 @@
 namespace LogXtreme.WinDsk.Infrastructure.Tests {
     internal class StandardNetEventSource {
 
+        private int invokations = 0;
+        private FinalizeTracker finalizeTracker;
+
         public event EventHandler Event = delegate { };
+
+        public StandardNetEventSource(ref FinalizeTracker finalizeTracker) {
+            this.finalizeTracker = finalizeTracker;
+        }
 
         public void Raise() {
             Event(this, EventArgs.Empty);
+        }
+
+        ~StandardNetEventSource() {
+            this.finalizeTracker.MarkAsFinalized();
         }
     }
 
