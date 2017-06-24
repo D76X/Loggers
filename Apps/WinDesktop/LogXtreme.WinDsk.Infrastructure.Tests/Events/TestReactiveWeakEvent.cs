ac31d19bad9eeb17e325db61902653a1bf82b0ff
Davide Spano <davide.spano.x@gmail.com>
2017-06-24 19:12:50 +0100

    TestReactiveWeakEvent class

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/Events/TestReactiveWeakEvent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogXtreme.WinDsk.Infrastructure.Tests.Events {
+
+    [TestClass]
+    public class TestReactiveWeakEvent {
+    }
+}
