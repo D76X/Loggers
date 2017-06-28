8084db9c23541339f467abf2be0b481a9cae1fee
Davide Spano <davide.spano.x@gmail.com>
2017-06-28 17:21:05 +0100

    added SlabManagementTools project to solution and InProcManagement class

---
+++ Apps/Instrumentation/SlabManagementTools/InProcSlabManagement.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using System;
+using System.Diagnostics.Tracing;
+
+namespace SlabManagementTools {
+
+    public class InProcSlabManagement {
+
+        private static ObservableEventListener listener = new ObservableEventListener();
+        private static EventSource eventSource;
+
+        public static void StartInProcTracing(EventSource source) {
+
+            if (eventSource != null) {
+                throw new InvalidOperationException(@"Only a single event source can be started");
+            }
+
+            eventSource = source;
+            listener.EnableEvents(eventSource, EventLevel.LogAlways, Keywords.All);
+            listener.LogToConsole();
+        }
+
+        public static void StopInProcTracing() {
+
+            if (eventSource == null) { return; }
+
+            listener.DisableEvents(eventSource);
+            listener.Dispose();
+            eventSource.Dispose();
+        }
+    }
+}
