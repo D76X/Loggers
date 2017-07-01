a564ce436299ae1faea90a2193806fded48a15b8
Davide Spano <davide.spano.x@gmail.com>
2017-07-01 13:58:04 +0100

    changed RegionNames added Workspace region

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -3,14 +3,17 @@ namespace LogXtreme.WinDsk.Infrastructure {
 
     public static class RegionNames {
 
-        public const string RegionContent = @"RegionContent";
-
         public const string RegionMainMenu = @"RegionMainMenu";
         public const string RegionToolbar = @"RegionToolbar";
-        public const string RegionLeftPanels = @"RegionLeftPanels";
-        public const string RegionRightPanels = @"RegionRightPanels";
-        public const string RegionStatusBar = @"RegionStatusBar";
 
+        public const string RegionContent = @"RegionContent";        
+        public const string RegionLeftPanels = @"RegionLeftPanels";
+        public const string RegionWorkspace = @"RegionWorkspace";
+        public const string RegionRightPanels = @"RegionRightPanels";
+
+        // normally in the left panel
         public const string RegionDeviceTree = @"RegionDeviceTree";
+
+        public const string RegionStatusBar = @"RegionStatusBar";
     }
 }
