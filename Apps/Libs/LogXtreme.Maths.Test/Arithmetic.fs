a228651035eb5d921763f4d07b401922c499d2f9
Davide Spano <davide.spano.x@gmail.com>
2017-07-10 22:57:23 +0100

    changed Arithmetics test changed F# notes

--- Apps/Libs/LogXtreme.Maths.Test/Arithmetic.fs
+++ Apps/Libs/LogXtreme.Maths.Test/Arithmetic.fs
@@ -4,5 +4,16 @@ open Xunit
 open LogXtreme.Maths.Arithmetic      
 
 [<Fact>]
-let ``add 1 to 2 gets 3``() = 
+let ``1 plus 2 gives 3``() = 
     Assert.Equal(3,1+2)
+
+
+[<Fact>]
+let ``add 1 to 2 gives 3``() = 
+    Assert.Equal(3,add 1 2)
+
+[<Theory>]
+[<InlineData(3, 5)>]
+[<InlineData(7, -2.6)>]
+let ``add x to y gives x + y`` x y =
+    Assert.Equal(x+y, add x y)
