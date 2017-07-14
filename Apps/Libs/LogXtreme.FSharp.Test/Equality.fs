acc1699c487e0cbf55f31078042993d5cbaa9cc7
Davide Spano <davide.spano.x@gmail.com>
2017-07-14 21:31:28 +0100

    added LogXtrem.FSharp.Test with some code ported from LogXtreme.Maths.Arithmetic

---
+++ Apps/Libs/LogXtreme.FSharp.Test/Equality.fs
@@ -0,0 +1,18 @@
+﻿module LogXtreme.FSharp.Test.Equality
+
+open Xunit
+open System.Numerics;
+
+(*structural equality in F#*)
+[<Fact>]
+let ``1 plus 2 gives 3``() = 
+    Assert.Equal(3,1+2)
+
+[<Fact>]
+let ``complex (1,j) = complex (1,j)`` = 
+    let c1 = Complex(1.,1.)
+    let c2 = Complex(1.,1.)
+    Assert.Equal(c1,c2)
+
+type Class1() = 
+    member this.X = "F#"
--- Apps/Libs/LogXtreme.Maths.Test/Arithmetic.fs
+++ Apps/Libs/LogXtreme.Maths.Test/Arithmetic.fs
@@ -1,19 +1,12 @@
 ﻿module LogXtreme.Maths.Test.Arithmetic
 
 open Xunit
-open LogXtreme.Maths.Arithmetic      
-
-[<Fact>]
-let ``1 plus 2 gives 3``() = 
-    Assert.Equal(3,1+2)
-
-
-[<Fact>]
-let ``add 1 to 2 gives 3``() = 
-    Assert.Equal(3,add 1 2)
+open LogXtreme.Maths.Arithmetic
 
 [<Theory>]
 [<InlineData(3, 5)>]
 [<InlineData(7, -2.6)>]
 let ``add x to y gives x + y`` x y =
     Assert.Equal(x+y, add x y)
+
+
