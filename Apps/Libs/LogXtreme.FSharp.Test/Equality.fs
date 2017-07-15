f934279ce8f663047f18179b7f6b99b7bb4c9502
Davide Spano <davide.spano.x@gmail.com>
2017-07-15 13:57:16 +0100

    changes to Equality,fs

--- Apps/Libs/LogXtreme.FSharp.Test/Equality.fs
+++ Apps/Libs/LogXtreme.FSharp.Test/Equality.fs
@@ -2,6 +2,7 @@
 
 open Xunit
 open System.Numerics;
+open System
 
 (*structural equality in F#*)
 [<Fact>]
@@ -12,7 +13,27 @@ let ``1 plus 2 gives 3``() =
 let ``complex (1,j) = complex (1,j)`` = 
     let c1 = Complex(1.,1.)
     let c2 = Complex(1.,1.)
+    let e = (c1 = c2)
+    Assert.True(e)
+    Assert.True((c1=c2))
     Assert.Equal(c1,c2)
 
-type Class1() = 
-    member this.X = "F#"
+type UserDetails = {
+    Id: Guid
+    UserName: string
+    Password: string
+    JoinDate: DateTime}
+
+[<Fact>]
+let ``UserDetails user1 = user2``
+    let u1 = {Id = Guid }
+
+
+//type Class1() = 
+//    member this.X = "F#"
+
+//[<Fact>]
+//let ``vector (1,j) = vector (1,j)`` = 
+//    let v1 = Vector
+
+
