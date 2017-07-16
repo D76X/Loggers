3b6a4877cd23bcd187dfeba03a80e8b217d6fe4b
Davide Spano <davide.spano.x@gmail.com>
2017-07-16 13:54:01 +0100

    changes to get the DataTreeViewModel to display data

--- Apps/WinDesktop/Modules/InfrastructureModules/DataTreeModule/ViewModels/DataTreeViewModel.cs
+++ Apps/WinDesktop/Modules/InfrastructureModules/DataTreeModule/ViewModels/DataTreeViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using DataTreeModule.Interfaces;
 using Prism.Mvvm;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace DataTreeModule.ViewModels {
     public class DataTreeViewModel : 
@@ -8,9 +10,29 @@ namespace DataTreeModule.ViewModels {
         IDataTreeViewModel, 
         IDisposable {
 
+        readonly ReadOnlyCollection<DataViewModel> _firstGeneration;
+        readonly DataViewModel _rootData;
+
         public DataTreeViewModel() {
-                
-        }      
+
+            var childModel = new DataModel() { Name = "child" };
+            var childData = new List<DataModel>();
+            childData.Add(childModel);
+            var rootData = new DataModel(childData);
+
+            _rootData = new DataViewModel(rootData);
+
+            _firstGeneration = new ReadOnlyCollection<DataViewModel>(
+                new DataViewModel[]
+                {
+                    _rootData
+                });
+
+        }
+
+        public ReadOnlyCollection<DataViewModel> FirstGeneration {
+            get { return _firstGeneration; }
+        }
 
         #region IDisposable
 
---
+++ Apps/WinDesktop/Modules/InfrastructureModules/DeviceTreeModule/Interfaces/IDeviceViewModel.cs
@@ -0,0 +1,8 @@
+﻿using LogXtreme.WinDsk.Infrastructure.Models;
+
+namespace DeviceTreeModule.Interfaces {
+    public interface IDeviceViewModel : IViewModel {
+
+        string Name { get; set; }
+    }
+}
