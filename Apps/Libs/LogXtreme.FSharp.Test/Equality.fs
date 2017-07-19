dff84649a069d684e06646beeb370d63c050bebc
Davide Spano <davide.spano.x@gmail.com>
2017-07-19 22:37:02 +0100

    changed Equality.fs added Unquote pckg to solution and to FSharp.Test.fsproj

--- Apps/Libs/LogXtreme.FSharp.Test/Equality.fs
+++ Apps/Libs/LogXtreme.FSharp.Test/Equality.fs
@@ -4,13 +4,23 @@ open Xunit
 open System.Numerics;
 open System
 
+type UserDetails = {
+    Id: Guid
+    UserName: string
+    Password: string
+    JoinDate: DateTime}
+
 (*structural equality in F#*)
 [<Fact>]
 let ``1 plus 2 gives 3``() = 
     Assert.Equal(3,1+2)
 
 [<Fact>]
-let ``complex (1,j) = complex (1,j)`` = 
+let ``2 plus 2 gives 4``() = 
+    Assert.Equal(4,2+2)
+
+[<Fact>]
+let ``complex 1,j = complex 1,j``()= 
     let c1 = Complex(1.,1.)
     let c2 = Complex(1.,1.)
     let e = (c1 = c2)
@@ -18,16 +28,22 @@ let ``complex (1,j) = complex (1,j)`` =
     Assert.True((c1=c2))
     Assert.Equal(c1,c2)
 
-type UserDetails = {
-    Id: Guid
-    UserName: string
-    Password: string
-    JoinDate: DateTime}
-
 [<Fact>]
-let ``UserDetails user1 = user2``
-    let u1 = {Id = Guid }
+let ``UserDetails user1 = user2``()= 
 
+    let u1 = {
+        Id = Guid "A738A7D1-8232-44FD-9296-852D856EE554"
+        UserName = "user1"
+        Password = "password1"
+        JoinDate = DateTime.MinValue} 
+
+    let u2 = {
+        Id = Guid "A738A7D1-8232-44FD-9296-852D856EE554"
+        UserName = "user1"
+        Password = "password1"
+        JoinDate = DateTime.MinValue} 
+
+    Assert.True((u1=u2))
 
 //type Class1() = 
 //    member this.X = "F#"
