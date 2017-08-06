779edbb13aeedaf0c40e795ef3b0f985540ed502
Davide Spano <davide.spano.x@gmail.com>
2017-08-06 22:07:47 +0100

    added TaskDelegateWorker and Test to Infrastructure

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/Services/TaskDelegateWorkerTest.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogXtreme.WinDsk.Infrastructure.Tests.Services {
+
+    [TestClass]
+    public class TaskDelegateWorkerTest {
+    }
+}
---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Services/TaskDelegateWorker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LogXtreme.WinDsk.Infrastructure.Services {
+
+    /// <summary>
+    /// Implementation of the Backgroundworker pattern based on TPL.
+    /// </summary>    
+    public class TaskDelegateWorker<TInput, TResult> :
+        IDelegateWorker<TInput, TResult> {
+
+        private Task<TResult> task;
+
+        public TaskDelegateWorker() {
+
+        }
+
+        public TaskDelegateWorker(
+           bool supportReportProgress = false,
+           bool supportCancellation = false) {
+
+        }
+
+        public void Process(
+        Func<TInput, IDelegateWorkerResult<TResult>> toExecute,
+        Action<IDelegateWorkerResult<TResult>> onComplete,
+        Action<IDelegateWorkerResult<TResult>> onCancelled) {
+
+            throw new NotImplementedException();
+        }
+
+        public void StartProcess(TInput initialInput) {
+
+            this.task.Start();
+        }
+    }
+}
