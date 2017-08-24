9197bbeec23d65642a4b981138ff7ba2a73a468d
Davide Spano <davide.spano.x@gmail.com>
2017-08-24 20:34:14 +0100

    changes to ExampleWrapperTypes1.fs

--- Apps/Libs/LogXtreme.ComputationExpressions/ExampleWrapperTypes1.fs
+++ Apps/Libs/LogXtreme.ComputationExpressions/ExampleWrapperTypes1.fs
@@ -33,8 +33,7 @@ let divideBy den num =
     else Some(num/den)
 
 let result = 
-    maybewf
-        {
+    maybewf {
         let! anInt = 3 |> divideBy 3
         let! anInt2 = 8 |> divideBy 4
         return anInt + anInt2 
@@ -64,10 +63,58 @@ type DbResult<'a> =
 type DbResultBuilder() = 
 
     // when f m returns Success then continue by applying teh next f to the result
-    // when f m retunrns Error then interrupt the chain of calls and returns the Error
+    // when f m retunrns Error then inte rrupt the chain of calls and returns the Error
     member this.Bind(m,f)=
         match m with 
         | Error _ -> m
         | Success a ->
             printfn "\tSuccessful: %s" a
             f a
+
+    // when the last f m is reaches the return wraps x back
+    // this suggests that between the last f m and the call to return something
+    // else happens that unwraps the value from the last f m in the chain 
+    member this.Return(x) = 
+        Success x    
+
+let dbresultwf = DbResultBuilder()
+
+// with such a computation expression builder we could chain calles to services 
+// where each may return a failure or a success and so that any failure in the 
+// chain shoukd result in teh failure of teh workflow with a message related to
+// the link where teh failure happened 
+let getCustomerId name =
+    if (name = "") 
+    then Error "getCustomerId failed"
+    else Success "Cust42"
+
+let getLastOrderForCustomer custId =
+    if (custId = "") 
+    then Error "getLastOrderForCustomer failed"
+    else Success "Order123"
+
+let getLastProductForOrder orderId =
+    if (orderId  = "") 
+    then Error "getLastProductForOrder failed"
+    else Success "Product456"
+
+// and finally the workflow that chains these calls to services
+let product = 
+    dbresultwf {
+    let! custId = getCustomerId "Alice"
+    let! orderId = getLastOrderForCustomer custId
+    let! productId = getLastProductForOrder orderId 
+    printfn "Product is %s" productId
+    return productId   
+    } |> printfn "%A"
+  
+// or for the failing case
+let productNotFound = 
+    dbresultwf {
+    let! custId = getCustomerId "Alice"
+    let! orderId = getLastOrderForCustomer "" // cause an error in the call chain 
+    let! productId = getLastProductForOrder orderId 
+    printfn "Product is %s" productId
+    return productId   
+    } |> printfn "%A"
+
