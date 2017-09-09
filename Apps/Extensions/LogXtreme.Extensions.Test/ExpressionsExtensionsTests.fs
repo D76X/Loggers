1c08075ca82e32de1ecfc2e49d5f7a3a7b383db0
Davide Spano <davide.spano.x@gmail.com>
2017-09-09 12:17:51 +0100

    added test for ExpressionExtension.ToProperty

--- Apps/Extensions/LogXtreme.Extensions.Test/ExpressionsExtensionsTests.fs
+++ Apps/Extensions/LogXtreme.Extensions.Test/ExpressionsExtensionsTests.fs
@@ -1,18 +1,31 @@
 ﻿module LogXtreme.FSharp.Test.ExpressionsExtensions
 
+// https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/functions/lambda-expressions-the-fun-keyword
+// https://stackoverflow.com/questions/45896796/pass-expressions-and-lambdas-parameter-to-c-sharp-extension-method-in-f
+
 open System
 open System.Numerics
 open Xunit
 open Swensen.Unquote
+open System.Linq.Expressions;
+open LogXtreme.Extensions
 
 open LogXtreme.Extensions
 
+type TestClass() = 
+    member val IdProperty: string = System.String.Empty with get, set
+
 [<Fact>]
-let ``failing test to rember to write tests for Expression.ToPropertyInfo``()=
+let ``Expression.ToPropertyInfo gets PropertyInfo for property on class instance``()=
 
     // arrange
+    let instance = TestClass()     
+    let readPropertyValue (i: TestClass) = i.IdProperty 
+    let x = (fun() -> readPropertyValue)
+    let propertyValue = readPropertyValue instance
 
     // act
+    let propertyInfo = ExpressionExtensions.ToPropertyInfo(x)
 
     // assert
     Assert.True(false)
