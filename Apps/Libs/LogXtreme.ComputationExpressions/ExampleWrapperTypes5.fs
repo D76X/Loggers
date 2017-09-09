7cea9093f4e0a7bb0305ac40a7fb97ebdebeb957
Davide Spano <davide.spano.x@gmail.com>
2017-09-09 12:18:24 +0100

    added ExampleWrapperType5.ts

---
+++ Apps/Libs/LogXtreme.ComputationExpressions/ExampleWrapperTypes5.fs
@@ -0,0 +1,13 @@
+module public LogXtreme.ComputationExpressions.Examples.ExampleWrapperTypes5
+
+// https://fsharpforfunandprofit.com/posts/computation-expressions-wrapper-types-part2/
+
+// Wrapper types are normally generic types such as the F# native type Option<'T> and custome types
+// such as DbResult<'T> which have similar semantic. However, wrapper type need not necessarly be 
+// generic types.
+
+// Can non-generic wrapper types work?
+// Can we use List<T> or IEnumerable<T> as wrapper types? 
+
+
+
