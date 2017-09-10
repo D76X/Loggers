ae89f042828b27951363ed8231e290cb6f38b694
Davide Spano <davide.spano.x@gmail.com>
2017-09-10 15:40:56 +0100

    added propertyName code quotation to read the property name ot GenericUtils.fs

--- Apps/Libs/LogXtreme.FSharp.Utils/GenericUtils.fs
+++ Apps/Libs/LogXtreme.FSharp.Utils/GenericUtils.fs
@@ -1,5 +1,8 @@
 ﻿module LogXtreme.FSharp.UtilsFunctions
 
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+
 // Efficient concatenation of objects into a string with string builder
 // https://stackoverflow.com/questions/18595597/is-using-a-stringbuilder-a-right-thing-to-do-in-f
 let strconc = 
@@ -16,6 +19,9 @@ strconc (["one"; "two"; 3 ]: list<obj>) |> printfn "%s"
 
 // Quotation to read the property name...
 // http://www.contactandcoil.com/software/dotnet/getting-a-property-name-as-a-string-in-f/
-
+let propertyName quotation = 
+    match quotation with 
+    | PropertyGet (_,propertyInfo,_) -> propertyInfo.Name
+    | _ -> System.String.Empty
                 
        
