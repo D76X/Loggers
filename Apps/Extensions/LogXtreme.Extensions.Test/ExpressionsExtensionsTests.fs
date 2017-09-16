05b805b32fe3327102398d576152366155628617
Davide Spano <davide.spano.x@gmail.com>
2017-09-16 16:28:28 +0100

    fixed tests for extension methods ExpressionExtensions.ToPropertyInfo and .ToPropertyName

--- Apps/Extensions/LogXtreme.Extensions.Test/ExpressionsExtensionsTests.fs
+++ Apps/Extensions/LogXtreme.Extensions.Test/ExpressionsExtensionsTests.fs
@@ -7,48 +7,59 @@ open System
 open System.Numerics
 open Xunit
 open Swensen.Unquote
+
 open System.Linq.Expressions;
 open LogXtreme.Extensions
-
-open LogXtreme.Extensions
+open LogXtreme.FSharp.UtilsFunctions
 
 type TestClass(pValue: string) = 
     member val IdProperty: string = pValue with get, set
 
 [<Fact>]
-let ``Expression.ToPropertyInfo gets PropertyInfo for property on class instance``()=
+let ``Expression.ToPropertyInfo and Expression.ToPropertyName work correctly``()=
 
     // arrange
     let expectedPropertyValue = @"testValue"
     let instance = TestClass(expectedPropertyValue) 
+    // different ways of readinf the value of a property of a class instance
     let readPropertyValue (i: TestClass) = i.IdProperty     
-    let lamba = fun() -> readPropertyValue       
+    let lamba = fun() -> readPropertyValue
+    let pValue = fun() -> instance.IdProperty
     
     // how do I pull out the name of a property in F#?  
+    
+    // this is teh crudest way possible
+    let expectedPropertyNameRaw = @"IdProperty" // YUK! 
+    
+    // this is the way that is done in F# 
     // http://www.contactandcoil.com/software/dotnet/getting-a-property-name-as-a-string-in-f/
-    let expectedPropertyName = "IdProperty" // YUK! 
+    let expectedPropertyName = propertyName <@ instance.IdProperty @>
 
     // act
-    // (fun (i: TestClass) -> readPropertyValue i) is the expression for a lambda to read the value of a property
-    // notice the brakets sourround the lambda expression as in (lamba) where lambda = fun (i: TestClass) -> readPropertyValue i
+    
+    // read the property value in all possible ways
     let directReadBackPropertyValue = instance.IdProperty
     let readPropertyValueViaFunction  = readPropertyValue instance
-    let readPropertyValueViaLambda  = lamba() instance    
+    let readPropertyValueViaLambda  = lamba() instance
+    let read_pValue = pValue()
     
-    // this is wrong and throws the exception for Member = null
-    //let propertyInfo = ExpressionExtensions.ToPropertyInfo((fun (i: TestClass) -> readPropertyValue i))
+    // exercise the extension method ExpressionExtensions.ToPropertyInfo
+    let actualPropertyInfo = ExpressionExtensions.ToPropertyInfo(fun (i: TestClass) -> i.IdProperty)   
     
+    // exercise the extension method ExpressionExtensions.ToPropertName
+    let actualPropertyName = ExpressionExtensions.ToPropertyName(fun (i: TestClass) -> i.IdProperty)   
+        
+    // test that the different ways of reading the value of a property produce the expected result
+    test<@ expectedPropertyValue = directReadBackPropertyValue @>
+    test<@ expectedPropertyValue = instance.IdProperty @>
+    test<@ expectedPropertyValue = readPropertyValueViaFunction @>
+    test<@ expectedPropertyValue = readPropertyValueViaLambda @>
+    test<@ expectedPropertyValue = read_pValue @>
 
-    // assert
+    // test that the propertyInfo instace from the extension method call has Name set to
+    // the expected property name of the class the property is defined on
+    test<@ expectedPropertyNameRaw = expectedPropertyName @>
+    test<@ expectedPropertyName = actualPropertyInfo.Name @>
+    test<@ expectedPropertyName = actualPropertyName @>
     
-    //Assert.True(propertyInfo :? System.Reflection.PropertyInfo) // MOOT POINT!
-    Assert.Equal(expectedPropertyValue, directReadBackPropertyValue)
-    Assert.Equal(expectedPropertyValue, instance.IdProperty)
-    Assert.Equal(expectedPropertyValue, readPropertyValueViaFunction)
-    Assert.Equal(expectedPropertyValue, readPropertyValueViaLambda)
-
-    // this is the main test
-    Assert.Equal(expectedPropertyName, propertyInfo.Name)
-    
-    Assert.True(false);
 
