2014d930ca7a414e4b011411ac0250de6bded367
Davide Spano <davide.spano.x@gmail.com>
2017-09-17 15:01:48 +0100

    added LogXtreme.WinDsk.TestDataGrid.csproj and some basic code

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.TestDataGrid/ViewModels/ItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LogXtreme.WinDsk.TestDataGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
+
+    public class ItemViewModel : INotifyPropertyChanged  {
+
+        private ItemModel model; 
+
+        public ItemViewModel(ItemModel model) {
+            this.model = model;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
