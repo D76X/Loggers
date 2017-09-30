384c985fe07bf4ddbd39047f4e12049ed4e820b7
Davide Spano <davide.spano.x@gmail.com>
2017-09-30 16:18:14 +0100

    added implementation RelayCommand to WinDsk.Infrastructure and added commands to demo TestDataGrid DataSourceVM

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LogXtreme.WinDsk.Infrastructure.Commands {
+
+    /// <summary> 
+    /// Standard implementation of the ICommand. This is additional to the implementations 
+    /// already available in the Prims library as DelegateCommand and the DelegateCommand<T>.
+    /// Genarally DelegateCommand and the DelegateCommand<T> from the Prism library should 
+    /// be used instead of RelayCommand and RelayCommand<T> but there are cases where these 
+    /// additional implementations are useful. Fr example in all demo applications that must
+    /// reference LogXtreme.WinDsk.Infrastructure and support Commands but that do not need 
+    /// any of the assets from Prims.
+    /// 
+    /// Refs
+    /// https://msdn.microsoft.com/en-us/library/gg405484(v=pandp.40).aspx
+    /// 
+    /// </summary>
+    public class RelayCommand : ICommand {
+
+        private Action targetExecute;
+        private Func<bool> targetCanExecute;
+
+        public RelayCommand(Action executeMethod) {
+            targetExecute = executeMethod;
+        }
+
+        public RelayCommand(Action executeMethod, Func<bool> canExecute) {
+            targetExecute = executeMethod;
+            targetCanExecute = canExecute;
+        }
+
+        public void RaiseCanExecuteChanged() {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        #region ICommand
+
+        /// <summary>
+        /// Should use weak reference - see Prism
+        /// </summary>
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        bool ICommand.CanExecute(object parameter) {
+
+            if (targetCanExecute != null) {
+                return targetCanExecute();
+            }
+
+            if (targetExecute != null) {
+                return true;
+            }
+
+            return false;
+        }
+
+        void ICommand.Execute(object parameter) {
+
+            targetExecute?.Invoke();
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// http://stackoverflow.com/questions/6273002/generic-type-safe-icommand-implementation
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RelayCommand<T> : ICommand {
+
+        private Action<T> targetExecute;
+        private Predicate<T> targetCanExecute;
+
+        public RelayCommand(Action<T> executeMethod) {
+            targetExecute = executeMethod;
+        }
+
+        public RelayCommand(Action<T> executeMethod, Predicate<T> canExecute) {
+            targetExecute = executeMethod;
+            targetCanExecute = canExecute;
+        }
+
+        #region ICommand
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public bool CanExecute(object parameter) {
+
+            if (targetCanExecute != null) {
+                return targetCanExecute((T)parameter);
+            }
+
+            if (targetExecute != null) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Execute(object parameter) {
+
+            targetExecute?.Invoke((T)parameter);
+        }
+        #endregion
+    }
+}
--- Apps/WinDesktop/LogXtreme.WinDsk.TestDataGrid/ViewModels/DataSourceViewModel.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.TestDataGrid/ViewModels/DataSourceViewModel.cs
@@ -1,16 +1,20 @@
-﻿using LogXtreme.WinDsk.TestDataGrid.Interfaces;
+﻿using LogXtreme.WinDsk.Infrastructure.Commands;
+using LogXtreme.WinDsk.TestDataGrid.Interfaces;
 using LogXtreme.WinDsk.TestDataGrid.Models;
 using System;
-using System.Collections;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
 
     /// <summary>
     /// 
     /// Refs
+    /// 
+    /// Commands
+    /// https://msdn.microsoft.com/en-us/library/gg405484(v=pandp.40).aspx
     /// 
     /// Dynamic DataGrid
     /// https://blogs.msmvps.com/deborahk/populating-a-datagrid-with-dynamic-columns-in-a-silverlight-application-using-mvvm/
@@ -24,21 +28,30 @@ namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
     /// INotifyPropertyChanged
     /// https://docs.microsoft.com/en-us/dotnet/framework/winforms/how-to-implement-the-inotifypropertychanged-interface
     /// </summary>
-    public class DataSourceViewModel: INotifyPropertyChanged {
+    public class DataSourceViewModel : INotifyPropertyChanged {
 
         private ObservableCollection<string> sampleHeader;
         private ObservableCollection<ISample> samples;
+        private RelayCommand cmdGetOneSample;
+        private ICommand cmdToggleSampleSource;
+        private bool readingSamples;
 
         public DataSourceViewModel() {
 
             this.sampleHeader = new ObservableCollection<string>();
             this.samples = new ObservableCollection<ISample>();
 
+            this.cmdGetOneSample = new RelayCommand(
+                this.ExecuteGetOneSample, 
+                this.CanExecuteGetOneSample);
+
+            this.cmdToggleSampleSource = new RelayCommand(this.ExecuteToggleSampleSource);
+
             this.sampleHeader.Add("CHN0");
             this.sampleHeader.Add("CHN1");
             this.sampleHeader.Add("CHN2");
 
-            this.samples.Add(new Sample(new string[3] {"2", "1", "2" }));
+            this.samples.Add(new Sample(new string[3] { "2", "1", "2" }));
             this.samples.Add(new Sample(new string[3] { "2", "10", "20" }));
             this.samples.Add(new Sample(new string[3] { "3", "100", "200" }));
         }
@@ -58,10 +71,41 @@ namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
             }
         }
 
+        private bool ReadingSamples {
+
+            get { return this.readingSamples;  }
+
+            set {
+
+                if(value != this.readingSamples) {
+                    this.readingSamples = value;
+                    this.cmdGetOneSample.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand CommandGetOneSample => this.cmdGetOneSample;
+        public ICommand CommandToggleSampleSource => this.cmdToggleSampleSource;
+
+        private bool CanExecuteGetOneSample() {
+            return !this.readingSamples;
+        }
+
+        private void ExecuteGetOneSample() { }        
+
+        private void ExecuteToggleSampleSource() {
+
+            this.ReadingSamples = !this.ReadingSamples;
+        }
+
+        #region INotifyPropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "") {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
     }
 }
