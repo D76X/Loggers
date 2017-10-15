f4a32a710b7c1f906e0db853c48f6833d5a5026e
Davide Spano <davide.spano.x@gmail.com>
2017-10-15 15:31:12 +0100

    added extension methods for Random and IEnumerable

---
+++ Apps/Extensions/LogXtreme.Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogXtreme.Extensions {
+
+    public static class IEnumerableExtensions {
+
+        /// <summary>      
+        /// Refs
+        /// https://stackoverflow.com/questions/9697332/how-can-i-override-tostring-method-for-all-ienumerableint32
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<string> Stringify<T>(this IEnumerable<T> ienumerable) {
+
+            return from v in ienumerable select v.ToString();
+        }
+    }
+}
---
+++ Apps/Extensions/LogXtreme.Extensions/RandomExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LogXtreme.Extensions {
+
+    /// <summary>    /// 
+    /// Refs
+    /// https://msdn.microsoft.com/en-us/library/system.random(v=vs.110).aspx    /// 
+    /// </summary>
+    public static class RandomExtensions {
+
+        public static int[] Sequence(
+            this Random generator, 
+            int length, 
+            int max, 
+            int min = 0) {
+
+            var randLock = new Object();
+
+            length = length >= 0 ? length : 0;           
+
+            var result = new int[length];
+
+            for (int i = 0; i < length; i++) {
+
+                lock (randLock) {
+                    result[i] = generator.Next(min, max);
+                }                
+            }
+
+            return result;
+        }      
+    }
+}
