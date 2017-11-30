54376e4f346e7cf6a1d0fb08cb9f1645f4369dfc
Davide Spano <davide.spano.x@gmail.com>
2017-11-30 14:09:47 +0000

    more unit tests for ResizeObservableCollection

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/Models/ResizeObservableCollectionTest.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure.Tests/Models/ResizeObservableCollectionTest.cs
@@ -6,7 +6,7 @@ using System;
 namespace LogXtreme.WinDsk.Infrastructure.Tests.Models {
 
     [TestClass]
-    public class ResizeObservableCollectionTest : TestBase {        
+    public class ResizeObservableCollectionTest : TestBase {
 
         [TestMethod]
         public void InfiniteSizeResizeObservableCollection_DefaultConstructor_DefaultBehaviour() {
@@ -21,7 +21,7 @@ namespace LogXtreme.WinDsk.Infrastructure.Tests.Models {
             Assert.AreEqual(collection.MaxSize, 0);
 
             Assert.AreEqual(
-                collection.Mode, 
+                collection.Mode,
                 ResizeObservableCollection<int>.CycleMode.None);
 
             Assert.AreEqual(collection.Count, 0);
@@ -105,7 +105,7 @@ namespace LogXtreme.WinDsk.Infrastructure.Tests.Models {
             // act
             collection.Add(first);
             collection.Add(second);
-            collection.Add(third);            
+            collection.Add(third);
 
             // assert 
             Assert.AreEqual(collection.Count, maxSize);
@@ -130,7 +130,7 @@ namespace LogXtreme.WinDsk.Infrastructure.Tests.Models {
             // assert    
             Assert.AreEqual(collection.Count, maxSize);
             Assert.IsFalse(collection.Contains(first));
-            Assert.IsFalse(collection.Contains(second));            
+            Assert.IsFalse(collection.Contains(second));
             Assert.AreEqual(collection.IndexOf(third), 0);
             Assert.AreEqual(collection.IndexOf(fourth), 1);
             Assert.AreEqual(collection.IndexOf(fifth), 2);
@@ -146,6 +146,200 @@ namespace LogXtreme.WinDsk.Infrastructure.Tests.Models {
             Assert.AreEqual(collection.IndexOf(fourth), 0);
             Assert.AreEqual(collection.IndexOf(fifth), 1);
             Assert.AreEqual(collection.IndexOf(sixth), 2);
+        }
+
+
+        [TestMethod]
+        public void InfiniteSizeResizeObservableCollection_SetMaxSize_SetMode_Queue() {
+
+            // arrange
+            int maxSize = 3;
+
+            var collection = new ResizeObservableCollection<int>(
+                maxSize,
+                ResizeObservableCollection<int>.CycleMode.Queue);
+
+            var first = 1;
+            var second = 2;
+            var third = 3;
+
+            var fourth = 4;
+            var fifth = 5;
+            var sixth = 6;
+
+            // act
+            collection.Add(first);
+            collection.Add(second);
+            collection.Add(third);
+
+            // assert 
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.AreEqual(collection.Mode, ResizeObservableCollection<int>.CycleMode.Queue);
+            Assert.AreEqual(collection.IndexOf(first), 0);
+            Assert.AreEqual(collection.IndexOf(second), 1);
+            Assert.AreEqual(collection.IndexOf(third), 2);
+
+            // act - first queue overflow
+            collection.Add(fourth);
+
+            // assert    
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.IsFalse(collection.Contains(first));
+            Assert.AreEqual(collection.IndexOf(second), 0);
+            Assert.AreEqual(collection.IndexOf(third), 1);
+            Assert.AreEqual(collection.IndexOf(fourth), 2);
+
+            // act - second queue overflow
+            collection.Add(fifth);
+
+            // assert    
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.IsFalse(collection.Contains(first));
+            Assert.IsFalse(collection.Contains(second));
+            Assert.AreEqual(collection.IndexOf(third), 0);
+            Assert.AreEqual(collection.IndexOf(fourth), 1);
+            Assert.AreEqual(collection.IndexOf(fifth), 2);
+
+            // act - third queue overflow
+            collection.Add(sixth);
+
+            // assert    
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.IsFalse(collection.Contains(first));
+            Assert.IsFalse(collection.Contains(second));
+            Assert.IsFalse(collection.Contains(third));
+            Assert.AreEqual(collection.IndexOf(fourth), 0);
+            Assert.AreEqual(collection.IndexOf(fifth), 1);
+            Assert.AreEqual(collection.IndexOf(sixth), 2);
+        }
+
+        [TestMethod]
+        public void InfiniteSizeResizeObservableCollection_SetMaxSize_SetMode_Flush() {
+
+            // arrange
+            int maxSize = 3;
+
+            var collection = new ResizeObservableCollection<int>(
+                maxSize,
+                ResizeObservableCollection<int>.CycleMode.Flush);
+
+            var first = 1;
+            var second = 2;
+            var third = 3;
+
+            var fourth = 4;
+            var fifth = 5;
+            var sixth = 6;
+
+            // act
+            collection.Add(first);
+            collection.Add(second);
+            collection.Add(third);
+
+            // assert 
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.AreEqual(collection.Mode, ResizeObservableCollection<int>.CycleMode.Flush);
+            Assert.AreEqual(collection.IndexOf(first), 0);
+            Assert.AreEqual(collection.IndexOf(second), 1);
+            Assert.AreEqual(collection.IndexOf(third), 2);
+
+            // act - first buffer overflow 
+            collection.Add(fourth);
+
+            // assert
+            Assert.AreEqual(collection.Count, 1);
+            Assert.AreEqual(collection.Mode, ResizeObservableCollection<int>.CycleMode.Flush);
+            Assert.IsFalse(collection.Contains(first));
+            Assert.IsFalse(collection.Contains(second));
+            Assert.IsFalse(collection.Contains(third));
+            Assert.AreEqual(collection.IndexOf(fourth), 0);
+
+            // act
+            collection.Add(fifth);
+
+            // assert
+            Assert.AreEqual(collection.Count, 2);
+            Assert.AreEqual(collection.IndexOf(fourth), 0);
+            Assert.AreEqual(collection.IndexOf(fifth), 1);
+
+            // act
+            collection.Add(sixth);
+
+            // assert
+            Assert.AreEqual(collection.Count, 3);
+            Assert.AreEqual(collection.IndexOf(fourth), 0);
+            Assert.AreEqual(collection.IndexOf(fifth), 1);
+            Assert.AreEqual(collection.IndexOf(sixth), 2);
+
+            // act - second overflow
+            collection.Add(first);
+            Assert.AreEqual(collection.Count, 1);
+            Assert.IsFalse(collection.Contains(fourth));
+            Assert.IsFalse(collection.Contains(fifth));
+            Assert.IsFalse(collection.Contains(sixth));
+            Assert.AreEqual(collection.IndexOf(first), 0);
+        }
+
+
+        [TestMethod]
+        public void InfiniteSizeResizeObservableCollection_SetMaxSize_SetMode_Roll() {
+
+            // arrange
+            int maxSize = 3;
+
+            var collection = new ResizeObservableCollection<int>(
+                maxSize,
+                ResizeObservableCollection<int>.CycleMode.Roll);
+
+            var first = 1;
+            var second = 2;
+            var third = 3;
+
+            var fourth = 4;
+            var fifth = 5;
+            var sixth = 6;
+
+            // act
+            collection.Add(first);
+            collection.Add(second);
+            collection.Add(third);
+
+            // assert 
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.AreEqual(collection.Mode, ResizeObservableCollection<int>.CycleMode.Roll);
+            Assert.AreEqual(collection.IndexOf(first), 0);
+            Assert.AreEqual(collection.IndexOf(second), 1);
+            Assert.AreEqual(collection.IndexOf(third), 2);
+
+            // act - first overflow
+            collection.Add(fourth);
+
+            // assert
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.IsFalse(collection.Contains(third));
+            Assert.AreEqual(collection.IndexOf(fourth), 0);
+            Assert.AreEqual(collection.IndexOf(first), 1);
+            Assert.AreEqual(collection.IndexOf(second), 2);
+
+            // act - second overflow
+            collection.Add(fifth);
+
+            // assert
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.IsFalse(collection.Contains(second));
+            Assert.AreEqual(collection.IndexOf(fifth), 0);
+            Assert.AreEqual(collection.IndexOf(fourth), 1);
+            Assert.AreEqual(collection.IndexOf(first), 2);
+
+            // act - third overflow
+            collection.Add(sixth);
+
+            // assert
+            Assert.AreEqual(collection.Count, maxSize);
+            Assert.IsFalse(collection.Contains(first));
+            Assert.AreEqual(collection.IndexOf(sixth), 0);
+            Assert.AreEqual(collection.IndexOf(fifth), 1);
+            Assert.AreEqual(collection.IndexOf(fourth), 2);
         }
     }
 }
