2a247ed0b50039e8334cc04e3a2fd90dd861dc44
Davide Spano <davide.spano.x@gmail.com>
2017-11-30 18:02:52 +0000

    renamed IEnumerable<T>.Stringify to IEnumerable<T>.StringifyItems and added   IEnumerable<T>.Stringify

--- Apps/Extensions/LogXtreme.Extensions/IEnumerableExtensions.cs
+++ Apps/Extensions/LogXtreme.Extensions/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace LogXtreme.Extensions {
 
@@ -10,9 +11,24 @@ namespace LogXtreme.Extensions {
         /// https://stackoverflow.com/questions/9697332/how-can-i-override-tostring-method-for-all-ienumerableint32
         /// </summary>
         /// <returns></returns>
-        public static IEnumerable<string> Stringify<T>(this IEnumerable<T> ienumerable) {
+        public static IEnumerable<string> StringifyItems<T>(this IEnumerable<T> ienumerable) =>
+            from v in ienumerable select v.ToString();
 
-            return from v in ienumerable select v.ToString();
-        }       
+        public static string Stringify<T>(
+            this IEnumerable<T> ienumerable,
+            string separator = StringExtentions.Empty) {
+            
+            var sb = new StringBuilder();
+
+            var length = ienumerable.Count();
+
+            for (int i = 0; i < length-1; i++) {
+                sb.Append($"{ienumerable.ElementAt(i)}{separator}");
+            }
+
+            sb.Append($"{ienumerable.ElementAt(length-1)}");
+
+            return sb.ToString();
+        }            
     }
 }
