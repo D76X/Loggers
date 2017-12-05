28f37bf9fd199eee78810d57527239fb2124257c
Davide Spano <davide.spano.x@gmail.com>
2017-12-05 15:52:35 +0000

    refactored TestSimpleEventLifecycle

--- Apps/Extensions/LogXtreme.Reactive.Extensions.Test.1/EventsTest.cs
+++ Apps/Extensions/LogXtreme.Reactive.Extensions.Test.1/EventsTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Reactive;
+using System.Reactive.Linq;
 
 namespace LogXtreme.Reactive.Extensions.Test._1 {
 
@@ -9,11 +11,11 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
 
         public TestEventArgs(string val) {
             this.Value = val;
-        }        
+        }
     }
 
     public class TestClassWithEvents {
-        
+
         public event EventHandler SimpleEvent;
         public event EventHandler<TestEventArgs> ComplexEvent;
 
@@ -31,31 +33,60 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
             this.ComplexEventInvokationCounter += 1;
             this.ComplexEvent?.Invoke(this, payload);
         }
+
+        public int? SimpleEventHandlersCount {
+            get => this.SimpleEvent?.GetInvocationList()?.Length;            
+        }
+
+        public int ComplexEventHandlersCount {
+            get => this.ComplexEvent.GetInvocationList().Length;
+        }
     }
 
     [TestClass]
     public class EventsTest {
 
         [TestMethod]
-        public void TestSimpleEventIsRaisedAndHandled() {
+        public void TestSimpleEventLifecycle() {
 
             // arrange 
-            var testInstance = new TestClassWithEvents();
+            var testInstance = new TestClassWithEvents();           
+            
+            // assert
+            Assert.AreEqual(0, testInstance.SimpleEventInvokationCounter);
+            Assert.IsNull(testInstance.SimpleEventHandlersCount);
 
+            // arrange
             bool simpleEventHandled = false;
 
             EventHandler handler = (object sender, EventArgs e) => {
                 simpleEventHandled = true;
             };
 
+            // act 
             testInstance.SimpleEvent += handler;
 
-            // act 
+            // assert
+            Assert.AreEqual(0, testInstance.SimpleEventInvokationCounter);
+            Assert.AreEqual(1, testInstance.SimpleEventHandlersCount);            
+
+            // act
             testInstance.RaiseSimpleEvent();
 
             // assert
             Assert.IsTrue(simpleEventHandled);
-            Assert.AreEqual(testInstance.SimpleEventInvokationCounter, 1);
+            Assert.AreEqual(1, testInstance.SimpleEventInvokationCounter);
+            Assert.AreEqual(1, testInstance.SimpleEventHandlersCount);
+
+            // act
+            testInstance.SimpleEvent -= handler;
+            Assert.IsTrue(simpleEventHandled);
+            Assert.AreEqual(1, testInstance.SimpleEventInvokationCounter);
+            Assert.IsNull(testInstance.SimpleEventHandlersCount);
+
+            // assert
+            testInstance.RaiseSimpleEvent();
+            Assert.AreEqual(2, testInstance.SimpleEventInvokationCounter);
         }
 
         [TestMethod]
@@ -67,7 +98,7 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
             bool complexEventHandled = false;
             string testValue = string.Empty;
             string expectedTestValue = @"test";
-            var payload = new TestEventArgs(expectedTestValue);            
+            var payload = new TestEventArgs(expectedTestValue);
 
             EventHandler<TestEventArgs> handler = (object sender, TestEventArgs e) => {
                 complexEventHandled = true;
@@ -83,6 +114,41 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
             Assert.IsTrue(complexEventHandled);
             Assert.AreEqual(testInstance.ComplexEventInvokationCounter, 1);
             Assert.AreEqual(testValue, expectedTestValue);
+        }
+
+        /// <summary>
+        /// Refs
+        /// http://www.introtorx.com/uat/content/v1.0.10621.0/04_CreatingObservableSequences.html#FromEvent
+        /// 
+        /// https://stackoverflow.com/questions/19895373/how-to-use-observable-fromevent-instead-of-fromeventpattern-and-avoid-string-lit
+        /// </summary>
+        [TestMethod]
+        public void TestFromEventPattern() {
+
+            // arrange 
+            var testInstance = new TestClassWithEvents();
+
+            // an observable of event patterns
+            var observable = Observable.FromEventPattern(
+                h => testInstance.SimpleEvent += h,
+                h => testInstance.SimpleEvent -= h);
+
+            // assert
+            //Assert.IsNull(testInstance.SimpleEvent);
+
+            var simpleEventWasCalled = false;
+            object sender = null;
+
+            // subscribe to the observable to 
+            var subscription = observable.Subscribe(eventPattern => {
+                sender = eventPattern.Sender;
+            });
+
+            // act 
+
+
+            // assert
+            Assert.Fail("test not implemented");
         }
     }
 }
