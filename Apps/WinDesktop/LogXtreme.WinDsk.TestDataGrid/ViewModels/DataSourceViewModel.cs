a8cb1bc36836ea5053e018a41eac89c7b5c466e1
Davide Spano <davide.spano.x@gmail.com>
2017-12-07 15:19:04 +0000

    refactored DataSourceViewModel in TestDataGrid added event subscriptions using the SubscribeWeakly pattern

--- Apps/WinDesktop/LogXtreme.WinDsk.TestDataGrid/ViewModels/DataSourceViewModel.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.TestDataGrid/ViewModels/DataSourceViewModel.cs
@@ -1,9 +1,11 @@
-﻿using LogXtreme.WinDsk.Infrastructure.Commands;
+﻿using LogXtreme.Reactive.Extensions;
+using LogXtreme.WinDsk.Infrastructure.Commands;
 using LogXtreme.WinDsk.TestDataGrid.Interfaces;
 using System;
 using System.ComponentModel;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
@@ -17,33 +19,46 @@ namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
 
         private IDisposable dataObsevable;
 
+        // subscriptions for events
+        private IDisposable eventSubscription_SubscribeToDataModelsObservable;
+        private IDisposable eventSubscription_DisposeSubscriptionToDataModelsObservable;
+
         private readonly RelayCommand<string> cmdStartReading;
         private readonly RelayCommand cmdStopReading;
 
-        private bool readingData;
+        private bool readingData;        
 
         public DataSourceViewModel(IDataSourceModel dataSourceModel) {
 
-            this.dataSourceModel = dataSourceModel;
+            this.dataSourceModel = dataSourceModel;            
 
-            //TODO replace with weak event pattern
-            this.dataSourceModel.OnStartDataReads += 
-                SubscribeToDataModelsObservable;
-
-            this.dataSourceModel.OnStopDataReads += 
-                DisposeSubscriptionToDataModelsObservable;
+            this.eventSubscription_SubscribeToDataModelsObservable = Observable
+                .FromEventPattern<IConnectableObservable<IDataModel>>(
+                    h => this.dataSourceModel.OnStartDataReads += h,
+                    h => this.dataSourceModel.OnStartDataReads -= h)
+                .SubscribeWeakly(
+                this,
+                (target, ep) => target.SubscribeToDataModelsObservable(ep.Sender, ep.EventArgs));
+            
+            this.eventSubscription_DisposeSubscriptionToDataModelsObservable = Observable
+                .FromEventPattern(
+                    h => this.dataSourceModel.OnStopDataReads += h,
+                    h => this.dataSourceModel.OnStopDataReads -= h)
+                .SubscribeWeakly(
+                this,
+                (target, ep) => target.DisposeSubscriptionToDataModelsObservable(ep.Sender, ep.EventArgs as EventArgs));            
 
             this.cmdStartReading = new RelayCommand<string>(
-                this.ExecuteStartReadingData,
-                this.CanExecuteStartReadingData);
+            this.ExecuteStartReadingData,
+            this.CanExecuteStartReadingData);
 
             this.cmdStopReading = new RelayCommand(
                 this.ExecuteStopReadingData,
-                this.CanExecuteStopReadingData);           
+                this.CanExecuteStopReadingData);
         }
 
         private void SubscribeToDataModelsObservable(
-            object sender, 
+            object sender,
             IObservable<IDataModel> e) {
 
             this.dataObsevable?.Dispose();
@@ -56,13 +71,13 @@ namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
                 .Subscribe(
                     d => { },
                     exc => { },
-                    () => {        
+                    () => {
                         this.dataSourceModel.StopDataReads();
-                    });           
+                    });
         }
 
         private void DisposeSubscriptionToDataModelsObservable(
-            object sender, 
+            object sender,
             EventArgs e) {
 
             this.dataObsevable?.Dispose();
@@ -92,7 +107,7 @@ namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
             this.cmdStartReading;
 
         public ICommand CommandStopReadingData =>
-            this.cmdStopReading;    
+            this.cmdStopReading;
         private void ExecuteStartReadingData(string countParam) {
 
             if (!int.TryParse(countParam, out int count) || count < 0) {
@@ -136,6 +151,12 @@ namespace LogXtreme.WinDsk.TestDataGrid.ViewModels {
 
                     this.dataObsevable?.Dispose();
                     this.dataObsevable = null;
+
+                    this.eventSubscription_SubscribeToDataModelsObservable?.Dispose();
+                    this.eventSubscription_SubscribeToDataModelsObservable = null;
+
+                    this.eventSubscription_DisposeSubscriptionToDataModelsObservable?.Dispose();
+                    this.eventSubscription_DisposeSubscriptionToDataModelsObservable = null;
                 }
 
                 // free unmanaged resources (unmanaged objects) and override a finalizer below.
