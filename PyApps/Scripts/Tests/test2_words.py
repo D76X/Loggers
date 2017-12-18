2a824e42b19d0ac358dd125bb0d2110e810aae5a
Davide Spano <davide.spano.x@gmail.com>
2017-12-18 12:34:54 +0000

    changes to the Python folder

---
+++ PyApps/Scripts/Tests/test2_words.py
@@ -0,0 +1,35 @@
+"""
+this is the docstring for this module
+"""
+from urllib.request import urlopen
+
+# this is a refactoring of the previous example
+# the function fetch_words produces the list
+# the print_words prints the list
+
+def fetch_words():
+    """fetches some text from a test URL and stick it into a list word by word"""
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+
+def print_words(story_words):
+    """prints a list of strings to the console"""
+    for word in story_words:
+        print(word)
+
+# if you want to test this module in teh REPL as if it were 
+# as script you can use this main() - it does not need to be
+# named main()
+def main():
+    """this is the main funtion of this module"""
+    words = fetch_words()
+    print_words(words)
+
+# make this module an executable script
+if __name__ == '__main__':
+    fetch_words()
