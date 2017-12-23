ec83c6129547422edf942ded9989507d44c16257
Davide Spano <davide.spano.x@gmail.com>
2017-12-23 12:04:58 +0000

    edited summaries and comments for RegionAdapterStackPanel

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Prism/RegionAdapterStackPanel.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Prism/RegionAdapterStackPanel.cs
@@ -4,12 +4,13 @@ using System.Windows;
 
 namespace LogXtreme.WinDsk.Infrastructure.Prism {
 
-    /// <summary>
-    /// 
+    /// <summary>    /// 
     /// A RegionAdapter<T> sits between the Region and the Host Control of type T.
     /// When Prism injects a view into or remove a view from a Region a RegionAdapter 
     /// provides the logic for the underlying control of type T for the Region. 
     /// 
+    /// Refs
+    /// https://app.pluralsight.com/player?course=prism-introduction&author=brian-lagunas&name=prism1-03-regions&clip=5&mode=live
     /// </summary>
     public class RegionAdapterStackPanel : RegionAdapterBase<StackPanel> {
 
@@ -18,11 +19,9 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
 
         }
 
-        /// <summary>
-        /// 
+        /// <summary> 
         /// Handles the logic that adds to or remove from the underlying host control a 
-        /// FrameworkElement instance that is the view that is injected into the region.       
-        /// 
+        /// FrameworkElement instance that is the view that is injected into the region.     
         /// </summary>
         /// <param name="region"></param>
         /// <param name="regionTarget"></param>
@@ -46,7 +45,8 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
                     // TODO: should handle the remove
 
                     // when a view is removed from the region the region adapter for the host control
-                    // that is a StackPanel must remove it from the Children collection
+                    // that is a StackPanel must remove it from the Children collection and perform any
+                    // housekeeping.
                 }
             };
         }
@@ -54,34 +54,29 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
         /// <summary>
         /// RegionAdapterBase<T>.CreateRegion returns an IRegion
         /// 
-        /// There exist 2 possible types of IRegion implementations that can be returned to the caller,
-        /// which one dependens on the type of Region as explained belo.
-        /// 
-        /// ---------------------------------------------------------------------------------------------
+        /// There exist 3 possible types of IRegion implementations that can be returned to the caller,
+        /// which one dependens on the type of Region as explained below.
+        ///         
         /// -1 SingleActiveRegion:	
         /// 
         /// Allows none or one ACTIVE view at any one time i.e. this implemenation of IRegion is returned  
         /// when ContentControl is the type T.
-        /// 
-        /// ---------------------------------------------------------------------------------------------        
+        ///                
         /// -2 AllActiveRegion 
         /// 
         /// All the views are ACTIVE and DEACTIVATION of views is NOT ALLOWED.
-        /// This implemenation of IRegion is returned  when ItemControl is the Type T.
-        /// 
-        /// ---------------------------------------------------------------------------------------------        
+        /// This implemenation of IRegion is returned  when ItemsControl is the Type T.
+        ///               
         /// -3 Region 
         /// 
-        /// Allows multiple active views.
-        /// This s returned when SelectorControl is the Type T.
-        /// 
-        /// ---------------------------------------------------------------------------------------------
+        /// Allows multiple active views. This s returned when SelectorControl is the Type T.
         /// 
         /// </summary>
         /// <returns>IRegion</returns>
         protected override IRegion CreateRegion() {
 
-            // for a StackPanel which might hold multiple items
+            // for a StackPanel which might hold multiple items all of which need to be 
+            // active at the same time
             return new AllActiveRegion();
         }
     }
