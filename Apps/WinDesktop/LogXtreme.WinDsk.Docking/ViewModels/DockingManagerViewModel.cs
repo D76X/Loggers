0ccb28b74a98f1c45c2358e55dc45b9bb20701df
Davide Spano <davide.spano.x@gmail.com>
2018-01-07 18:31:35 +0000

    changes to WinDsk.Docking

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Docking/ViewModels/DockingManagerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogXtreme.WinDsk.Docking.ViewModels {
+
+    public class DockingManagerViewModel {
+    }
+}
---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Docking/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogXtreme.WinDsk.Docking.ViewModels {
+    public class MainWindowViewModel {
+    }
+}
