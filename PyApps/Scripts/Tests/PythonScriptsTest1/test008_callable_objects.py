dbc5b482c6eb3bfb3b4cc25d3c082a9f2f28877c
Davide Spano <davide.spano.x@gmail.com>
2018-01-10 21:30:17 +0000

    changes to PyApps

--- PyApps/Scripts/Tests/PythonScriptsTest1/test008_callable_objects.py
+++ PyApps/Scripts/Tests/PythonScriptsTest1/test008_callable_objects.py
@@ -2,7 +2,15 @@
 This module illustrates the definition and the use of a callable objetcs.
 
 Usage:
-    Run...
+    In the REPL
+    >>> import test008_callable_objects
+    >>> test008_callable_objects.time_resolver_without_and_with_caching()
+    0.5042847158598106
+    0.48669231602153795
+    0.4976048976097829
+    0.49145116699946656
+    >>> print(test008_callable_objects.testResolverInstance._cache)
+    {'google.com': '216.58.204.14', 'sixty-north.com': '93.93.131.30'}
 """
 
 import socket  
@@ -21,6 +29,14 @@ class Resolver:
         if host not in self._cache:
             self._cache[host] = socket.gethostbyname(host)
         return self._cache[host]
+
+    def clear(self):
+        """Clears the cache."""
+        self._cache.clear()
+
+    def has_host(self, host):
+        """Checks whether a host is alrrady in the cache."""
+        return host in self._cache
 
 def test_resolver():
     """Module level function to test the module."""
@@ -42,4 +58,9 @@ def time_resolver_without_and_with_caching():
     print(t1)
     t2 = timeit.timeit("t.testResolverInstance('google.com')", setup="from __main__ import test008_callable_objects as t")
     print(t2)
+    t3 = timeit.timeit("t.testResolverInstance('sixty-north.com')", setup="from __main__ import test008_callable_objects as t")
+    print(t3)
+    t4 = timeit.timeit("t.testResolverInstance('sixty-north.com')", setup="from __main__ import test008_callable_objects as t")
+    print(t4)
+    pp.pprint(testResolverInstance._cache)
         
