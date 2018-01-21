653630e56b010e07b436b08bc14ee55ce08cebdb
Davide Spano <davide.spano.x@gmail.com>
2018-01-21 15:11:27 +0000

    changes to PyApps

---
+++ PyApps/Scripts/Tests/PythonScriptsTest1/test022_returning_functions.py
@@ -0,0 +1,15 @@
+"""
+This module illustrates functions from functions in Python.
+
+Usage:
+    In the REPL import the module and run the module level test function.
+
+    import test022_returning_functions as t
+    t.test_module()
+     
+"""   
+
+def test_module():
+    """Module-level tests."""
+    
+    
