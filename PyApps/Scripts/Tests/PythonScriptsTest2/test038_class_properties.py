e794ed8694a8a8973121f9f81ac674a54ebfcd6d
Davide Spano <davide.spano.x@gmail.com>
2018-02-16 21:52:20 +0000

    changes to PyApps

--- PyApps/Scripts/Tests/PythonScriptsTest2/test038_class_properties.py
+++ PyApps/Scripts/Tests/PythonScriptsTest2/test038_class_properties.py
@@ -16,7 +16,31 @@ Usage:
 class Temperature:
     """
     A class that models a measure of temperature.
-    """    
+    """  
+    
+    @staticmethod
+    def celsius_to_kelvin(c):
+        return c + 273.15 
+
+    @staticmethod
+    def kelvin_to_celsius(k):
+        return k - 273.15
+
+    @staticmethod
+    def farhenheit_to_kelvin(f):
+        return round((f - 32)*5/9 + 273.15, 2)
+
+    @staticmethod
+    def kelvin_to_fahrenheit(k):
+        return round((k - 273.15)*9*0.20 + 32, 2)    
+
+    @staticmethod
+    def fahrenheit_to_celsius(f):
+        return round((f-32)*5/9, 2)
+
+    @staticmethod
+    def celsius_to_fahrenheit(c):
+        return round(c*9/5+32, 2)
 
     def __init__(self):
         self._kelvin = 0
@@ -37,25 +61,25 @@ class Temperature:
 
     @property
     def celsius(self):
-        return round(self._kelvin - 273.15,2)
+        return Temperature.kelvin_to_celsius(self._kelvin)
 
     @celsius.setter
     def celsius(self, value):
         if (value < -273.15):
             raise ValueError("temp in C cannot be lower than -273.15 C.") 
-        self._kelvin = value + 273.15 
+        self._kelvin = Temperature.celsius_to_kelvin(value) 
 
     @property
     def fahrenheit(self):
         #1/5=0.20
-        return round((self._kelvin - 273.15)*9*0.20 + 32, 2)
+        return Temperature.kelvin_to_fahrenheit(self._kelvin)
 
     @fahrenheit.setter
     def fahrenheit(self, value):
         if(value < -459.67):
             raise ValueError("temp in F cannot be lower than -459.67 F.")
         #1/9=0.11
-        self._kelvin = ((value - 32)*5)/9 + 273.15
+        self._kelvin = Temperature.farhenheit_to_kelvin(value)
 
     def print(self):
         return "temp {K} K = {C} C = {F} F".format(K=self.kelvin, C=self.celsius, F=self.fahrenheit)
