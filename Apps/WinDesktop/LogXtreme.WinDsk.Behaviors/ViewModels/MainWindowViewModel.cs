c8eb2f9328630836d482a8716bc86977e1d8eca6
Davide Spano <davide.spano.x@gmail.com>
2018-02-17 14:04:49 +0000

    WinDsk.Behaviors MainWindowViewModel added Tabs VM

--- Apps/WinDesktop/LogXtreme.WinDsk.Behaviors/ViewModels/MainWindowViewModel.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Behaviors/ViewModels/MainWindowViewModel.cs
@@ -11,9 +11,15 @@ namespace LogXtreme.WinDsk.TestBehaviors.ViewModels {
 
             // Tab1
             this.viewModels.Add(new Tab1ViewModel());
+
+            // Tab2
+            this.viewModels.Add(new Tab2ViewModel());
         }
 
         public Tab1ViewModel Tab1ViewModel =>
             (Tab1ViewModel)this.viewModels[0];
+
+        public Tab2ViewModel Tab2ViewModel =>
+            (Tab2ViewModel)this.viewModels[1];
     }
 }
