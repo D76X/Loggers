ef9f7ccf2f77c5517726678ec60b2684c90440e4
Davide Spano <davide.spano.x@gmail.com>
2018-02-17 14:08:40 +0000

    changes to TestBehaviors added code to use KeyboardFocusBehavor and WatermarkBehavior

---
+++ Apps/WinDesktop/LogXtreme.WinDsk.Behaviors/ViewModels/Tab2ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using LogXtreme.WinDsk.Infrastructure.Wpf;
+
+namespace LogXtreme.WinDsk.TestBehaviors.ViewModels {
+
+    public class Tab2ViewModel : NotifyPropertyChangedBase {
+
+        private string message = @"Message from View Model";
+
+        public Tab2ViewModel() { }        
+
+        public string Message {
+            get => this.message;
+            set => this.SetProperty<string>(ref this.message, value);
+        }
+    }
+}
