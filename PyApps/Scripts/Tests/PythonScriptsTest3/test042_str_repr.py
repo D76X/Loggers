3324cc35c92ddc24bf579beec05d36ba1389ec1c
Davide Spano <davide.spano.x@gmail.com>
2018-02-27 10:29:07 +0000

    added PythonScriptsTest3 project to sln and new vs setting

---
+++ PyApps/Scripts/Tests/PythonScriptsTest3/test042_str_repr.py
@@ -0,0 +1,12 @@
+"""
+This module illustrates the use of __str__ and __repr__ in Python.
+"""
+
+class Point2D:
+    """A class that models a point on a geometrical plane."""
+    
+    def __init__(self, x, y):
+        self._
+
+def test_module():
+    """Module-level tests."""
