2065c31655e6a619e212f35f1a01e0fc3c591df7
Davide Spano <davide.spano.x@gmail.com>
2018-03-10 17:04:42 +0000

    WinDsk.Infrastructure.RegionNames added new region names.

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -9,7 +9,12 @@ namespace LogXtreme.WinDsk.Infrastructure {
         public const string RegionContent = @"RegionContent";        
         public const string RegionLeftPanels = @"RegionLeftPanels";
         public const string RegionWorkspace = @"RegionWorkspace";
-        public const string RegionRightPanels = @"RegionRightPanels";        
+        public const string RegionRightPanels = @"RegionRightPanels";
+
+        public const string RegionDataGrid = @"RegionDataGrid";
+        public const string RegionDataTree = @"RegionDataTree";
+        public const string RegionDeviceTree = @"RegionDeviceTree";
+        public const string RegionPlotting = @"RegionPlotting";
 
         public const string RegionStatusBar = @"RegionStatusBar";
     }
