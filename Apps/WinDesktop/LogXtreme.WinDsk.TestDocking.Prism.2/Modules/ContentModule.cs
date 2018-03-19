d84e3bc304a42e48dbeaa9d03e8766f29e60c25c
Davide Spano <davide.spano.x@gmail.com>
2018-03-19 14:07:14 +0000

    TestDocking.Prism.2\Modules\ContentModule register the ContentView with the content region and with the Prism navigation plumming. This view is navigated to by default by the ShellService when a new shell is created.

--- Apps/WinDesktop/LogXtreme.WinDsk.TestDocking.Prism.2/Modules/ContentModule.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.TestDocking.Prism.2/Modules/ContentModule.cs
@@ -1,7 +1,8 @@
-﻿using LogXtreme.WinDsk.TestDocking.Prism.Interfaces;
+﻿using LogXtreme.WinDsk.Infrastructure;
+using LogXtreme.WinDsk.Infrastructure.Unity;
+using LogXtreme.WinDsk.TestDocking.Prism.Interfaces;
 using LogXtreme.WinDsk.TestDocking.Prism.ViewModels;
 using LogXtreme.WinDsk.TestDocking.Prism.Views;
-using LogXtreme.WinDsk.Infrastructure;
 using Microsoft.Practices.Unity;
 using Prism.Modularity;
 using Prism.Regions;
@@ -30,8 +31,12 @@ namespace LogXtreme.WinDsk.TestDocking.Prism.Modules {
 
             // Register Shared Services
 
-            // Compose Views into the Shell
+            // Compose Views into the Shell when the module is initialized
             regionManager.RegisterViewWithRegion(RegionNames.RegionContent, typeof(ContentView));
+
+            // Register the View for navigation with Prism as the content view is navigated to
+            // when a second shell is created.
+            this.container.RegisterTypeForNavigation<ContentView>();
         }
     }
 }
