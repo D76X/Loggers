b425b7d82286fe479f23b0b741d1cd9914b73b28
Davide Spano <davide.spano.x@gmail.com>
2018-03-19 14:22:12 +0000

    changes to TestDocking.Prism.2\ViewModules\ContentViewModel implement IRegionManagerAware for navigation with scoped region manager and  OnNavigatedTo added bound properties Anchorables and Documents and ActiveContent to drive the data to the DockingManager.

--- Apps/WinDesktop/LogXtreme.WinDsk.TestDocking.Prism.2/ViewModels/ContentViewModel.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.TestDocking.Prism.2/ViewModels/ContentViewModel.cs
@@ -1,15 +1,54 @@
-﻿using LogXtreme.WinDsk.TestDocking.Prism.Interfaces;
-using Prism.Mvvm;
+﻿using LogXtreme.WinDsk.Infrastructure.Models;
+using LogXtreme.WinDsk.Infrastructure.Prism;
+using LogXtreme.WinDsk.TestDocking.Prism.Interfaces;
+using Prism.Regions;
 using System;
+using System.Collections.ObjectModel;
 
 namespace LogXtreme.WinDsk.TestDocking.Prism.ViewModels {
 
-    public class ContentViewModel : 
-        BindableBase, 
-        IContentViewModel, 
+    public class ContentViewModel :
+        ViewModelBase, 
+        IContentViewModel,
+        IRegionManagerAware,
         IDisposable {
 
-        public ContentViewModel() { }
+        private object activeContent;
+
+        public ContentViewModel() {
+
+            this.Documents = new ObservableCollection<object>();
+            this.Anchorables = new ObservableCollection<object>();
+        }
+
+        private IRegionManager scopedRegionManager;
+        public IRegionManager RegionManager {
+            get => this.scopedRegionManager;
+            set => this.scopedRegionManager = value;
+        }
+
+        public ObservableCollection<object> Anchorables {
+            get;
+            set;
+        }
+
+        public ObservableCollection<object> Documents {
+            get;
+            set;
+        }
+
+        public object ActiveContent {
+
+            get => this.activeContent;
+
+            set {
+                this.SetProperty<object>(ref activeContent, value);
+            }
+        }       
+
+        public override void OnNavigatedTo(NavigationContext navigationContext) {
+            base.OnNavigatedTo(navigationContext);
+        }
 
         #region IDisposable
 
@@ -24,7 +63,6 @@ namespace LogXtreme.WinDsk.TestDocking.Prism.ViewModels {
                 // dispose of subcriptions, etc.               
             }
         }
-
         #endregion
     }
 }
