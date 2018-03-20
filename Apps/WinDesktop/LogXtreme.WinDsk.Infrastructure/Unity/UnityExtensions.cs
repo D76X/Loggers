81c828732ffe3d67363e18d88398de6a46dcfb54
Davide Spano <davide.spano.x@gmail.com>
2018-03-20 11:38:04 +0000

    typo

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Unity/UnityExtensions.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Unity/UnityExtensions.cs
@@ -7,7 +7,7 @@ namespace LogXtreme.WinDsk.Infrastructure.Unity {
         /// <summary>
         /// This extension method is to make it easier and more explicit to register views 
         /// and view models types for navigation via Prism mavigation framework with the 
-        /// DI container. Notice that the navigatiuon name typeof(T).FullName is the fully
+        /// DI container. Notice that the navigation name typeof(T).FullName is the fully
         /// qualified name of the type.
         /// </summary>
         /// <typeparam name="T">The type of a view or view model to navigate to</typeparam>
