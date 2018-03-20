f61ed8185f8ea0a381b39869bf824e242939cc04
Davide Spano <davide.spano.x@gmail.com>
2018-03-20 11:34:50 +0000

    changes to TestDocking.Prism.2 changes to ContetView.xaml.cs this view MUST implent IRegionManagerAware and hook up handlers to its Loaded event to overcome two problems 1) when a second shell is instantiated the views and the view models in the second shell do not get their IRegionManagerAware.RegionManager aware set by the RegionManagerAwareBehavior because the RegionManagerAwareBehavior.ActiveViewsCollectionChanged is not invoked when the ScopedRegionNavigationContentLoader runs. 2) The ContetView.xaml contains an istance of the Avalon DockingManager as a region with a LayoutAnchorable set a child region for the DeviceTreeView but this must be registered with the scoped RegionManger of the containing shell for navigation to it to work.

--- Apps/WinDesktop/LogXtreme.WinDsk.TestDocking.Prism.2/Views/ContentView.xaml.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.TestDocking.Prism.2/Views/ContentView.xaml.cs
@@ -13,27 +13,94 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LogXtreme.WinDsk.Infrastructure.Models;
+using LogXtreme.WinDsk.Infrastructure.Prism;
 using LogXtreme.WinDsk.TestDocking.Prism.Interfaces;
+using Prism.Regions;
+using PrismRegions = Prism.Regions;
 
 namespace LogXtreme.WinDsk.TestDocking.Prism.Views {
     /// <summary>
     /// Interaction logic for ContentView.xaml
     /// </summary>
-    public partial class ContentView : UserControl, IContentView {
-
+    public partial class ContentView : 
+        UserControl, 
+        IContentView, 
+        IRegionManagerAware {
+        
         public ContentView(IContentViewModel viewModel) {
+
             InitializeComponent();
             this.ViewModel = viewModel;
+
+            this.Loaded += SetRegionManager;
+            this.Loaded += SetRegionManagerOnChildRegions;
+        }        
+
+        IRegionManager regionManager;
+        public IRegionManager RegionManager {
+
+            get => this.regionManager;
+
+            set {
+
+                this.regionManager = value;
+                
+                // set the region manager also on the view model if it has not yet been done.
+                var regionManagerAwareViewModel = this.ViewModel as IRegionManagerAware;
+                if (regionManagerAwareViewModel == null) { return; }
+                regionManagerAwareViewModel.RegionManager = 
+                    regionManagerAwareViewModel.RegionManager ??
+                    value;
+            }
         }
 
         public IViewModel ViewModel {
-            get {
-                return (IContentViewModel)this.DataContext;
-            }
+            get => (IContentViewModel)this.DataContext;
+            set => this.DataContext = value;
+        }
+        
+        /// <summary>
+        /// Sets the RegionManager on the view and its view model if 
+        /// IRegionManagerAware is supported and the RegionManager has not 
+        /// yet been set. The instance of the region manager used is found
+        /// on the shell.
+        /// 
+        /// Refs
+        /// https://stackoverflow.com/questions/302839/wpf-user-control-parent
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SetRegionManager(object sender, RoutedEventArgs e) {
 
-            set {
-                this.DataContext = value;
-            }
+            Window parentWindow = Window.GetWindow(this);
+
+            var containingShell = parentWindow as IShellView;
+
+            if (containingShell == null) { return; }
+
+            var regionManaggerAwareShellViewModel = containingShell.ViewModel as IRegionManagerAware;
+
+            if (regionManaggerAwareShellViewModel == null) { return; }
+
+            var scopedRegionManager = regionManaggerAwareShellViewModel.RegionManager;
+
+            this.RegionManager = this.RegionManager ?? scopedRegionManager;
+        }
+
+        /// <summary>
+        /// This is necessary to make sure that navigation to child regions declared 
+        /// within the DockingManager can be navigated to in Prism. The RegionManager
+        /// within the scope of the view must be told about the child regions for the
+        /// navigation to work.
+        /// 
+        /// Refs
+        /// https://stackoverflow.com/questions/44577082/prism-6-region-manager-requestnavigate-fails-to-navigate-for-some-regions
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SetRegionManagerOnChildRegions(object sender, RoutedEventArgs e) {
+
+            PrismRegions.RegionManager.SetRegionManager(deviceTreeName, this.RegionManager);
         }
     }
 }
