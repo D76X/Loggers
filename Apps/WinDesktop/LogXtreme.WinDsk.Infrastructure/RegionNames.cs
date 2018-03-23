273ae636f27f2798b1a5924d58c3aca52a3d91b2
Davide Spano <davide.spano.x@gmail.com>
2018-03-23 12:31:14 +0000

    changes to RegionNames added region names for DockingManager, DocumentPane in the docking  manager the DataTree and DeviceTree.

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace LogXtreme.WinDsk.Infrastructure {    
+namespace LogXtreme.WinDsk.Infrastructure {
 
-    public static class RegionNames {
+    public static class RegionNames {        
 
         public const string RegionMainMenu = @"RegionMainMenu";
         public const string RegionToolbar = @"RegionToolbar";
@@ -11,11 +11,10 @@ namespace LogXtreme.WinDsk.Infrastructure {
         public const string RegionWorkspace = @"RegionWorkspace";
         public const string RegionRightPanels = @"RegionRightPanels";
 
-        public const string RegionDataGrid = @"RegionDataGrid";
+        public const string RegionDockingManager = @"RegionDockingManager";
+        public const string RegionDocumentPane = @"RegionDocumentPane";
         public const string RegionDataTree = @"RegionDataTree";
         public const string RegionDeviceTree = @"RegionDeviceTree";
-        public const string RegionPlotting = @"RegionPlotting";
-
-        public const string RegionStatusBar = @"RegionStatusBar";
+        public const string RegionStatusBar = @"RegionStatusBar";        
     }
 }
