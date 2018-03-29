c3542e24ea4c6a3f9819bfb1638ba75e2da8bf46
Davide Spano <davide.spano.x@gmail.com>
2018-03-29 14:23:29 +0100

    changed default namespaces to LogXtreme.WinDsk.Modules.Services in service modules

--- Apps/WinDesktop/Modules/AppServices/MenuServiceModule/Properties/Resources.Designer.cs
+++ Apps/WinDesktop/Modules/AppServices/MenuServiceModule/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MenuServiceModule.Properties {
+namespace LogXtreme.WinDsk.Modules.Services.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MenuServiceModule.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MenuServiceModule.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LogXtreme.WinDsk.Modules.Services.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
