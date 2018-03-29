f0f40e04f84bc9775443db569e1718e24fffbd89
Davide Spano <davide.spano.x@gmail.com>
2018-03-29 14:33:59 +0100

    changes to RegionAdapterLayoutAnchorable

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Prism/RegionAdapterLayoutAnchorable.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Prism/RegionAdapterLayoutAnchorable.cs
@@ -1,8 +1,12 @@
-﻿using LogXtreme.Infrastructure.ContractValidators;
+﻿using LogXtreme.Ifrastructure.Enums;
+using LogXtreme.Infrastructure.ContractValidators;
+using LogXtreme.WinDsk.Infrastructure.Events;
+using LogXtreme.WinDsk.Infrastructure.Services;
 using Prism.Regions;
 using System;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Windows;
 using Xceed.Wpf.AvalonDock.Layout;
 
 namespace LogXtreme.WinDsk.Infrastructure.Prism {
@@ -16,22 +20,32 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
     /// <see cref="LayoutAnchorable"/>. 
     /// 
     /// It provides the logic to add or remove FrameworkElements from the underilying 
-    /// control when views are injected or removed from the region respectively. This
-    /// is done in the override <see cref="Adapt"/>.
+    /// control when views are injected or removed from the region respectively. It 
+    /// also hanles the upkeeping of the active views for the region in conjuction 
+    /// with the evemts of addition and removal of views from the region. 
+    /// This logic is implemented in the form of event hanlders attached to the relevant
+    /// events on the region's collections of views and active views. 
+    /// All of this is done in the override  <see cref="Adapt"/>.
     /// 
     /// It also tells Prism what kind of region it is. In this case the override 
     /// <see cref="CreateRegion"/> returns an instance of <see cref="SingleActiveRegion"/>
     /// to let the Prism framework know that in this region only one wie may be 
     /// active at any given time.
+    ///     
     /// 
     /// Refs
     /// http://avalondock.codeplex.com/discussions/390255
     /// </summary>
     public class RegionAdapterLayoutAnchorable : RegionAdapterBase<LayoutAnchorable> {
 
-        public RegionAdapterLayoutAnchorable(IRegionBehaviorFactory regionBehaviorFactory)
+        private IAvalonDockService avalonDockService;
+
+        public RegionAdapterLayoutAnchorable(
+            IRegionBehaviorFactory regionBehaviorFactory,
+            IAvalonDockService avalonDockService)
             : base(regionBehaviorFactory) {
 
+            this.avalonDockService = avalonDockService;
         }
 
         /// <summary>
@@ -50,8 +64,12 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
                 throw new InvalidOperationException();
             }
 
+            this.avalonDockService.RegisterRegionName<LayoutAnchorable>(region.Name);
+
             // monitor when views are injectedinto or removed from the region.
             region.Views.CollectionChanged += (s, e) => {
+
+                UIElement childUiElement = s as UIElement;
 
                 if (e.Action == NotifyCollectionChangedAction.Add &&
                                 region.ActiveViews.Count() == 0) {
@@ -59,21 +77,30 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
                     // a view has been injeted into the region.
                     // the injected view must be set to be the active view for the region.
                     // this type of region can only have one view in it at any time.
-                    region.Activate(e.NewItems[0]);
+                    var lastAddedView = e.NewItems[0];
+                    region.Activate(lastAddedView);
                 }
                 else if (e.Action == NotifyCollectionChangedAction.Remove) {
-
-                    // Is this right?
-                    // test it!
                     region.Deactivate(e.OldItems[0]);
                     region.Remove(e.OldItems[0]);
                 }
+
+                this.avalonDockService
+                    .RaiseDockingManagerChanged(this, new AvalonDockEventArgs(
+                        e, 
+                        AvalonDockEventEnum.ViewsCollectionChanged));         
             };
 
             region.ActiveViews.CollectionChanged += (s, e) => {
 
-                regionTarget.Content = region.ActiveViews.FirstOrDefault();
-            };
+                var newContent = region.ActiveViews.FirstOrDefault();
+                regionTarget.Content = newContent;
+
+                this.avalonDockService
+                    .RaiseDockingManagerChanged(this, new AvalonDockEventArgs(
+                        e,
+                        AvalonDockEventEnum.ActiveViewsCollectionChanged));
+            };            
         }
 
         /// <summary>
@@ -81,6 +108,6 @@ namespace LogXtreme.WinDsk.Infrastructure.Prism {
         /// </summary>
         /// <returns><see cref="IRegion"/></returns>
         protected override IRegion CreateRegion() =>
-            new SingleActiveRegion();
+            new SingleActiveRegion();      
     }
 }
