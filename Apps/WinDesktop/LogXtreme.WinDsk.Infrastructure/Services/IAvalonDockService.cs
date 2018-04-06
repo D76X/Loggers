f19b6af03f21f00df257d3bd41a9c214b02c555a
Davide Spano <davide.spano.x@gmail.com>
2018-04-06 14:11:08 +0100

    chnages to IAValonDockService and implementation added dictionary of parts - changes to projects with renames.

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Services/IAvalonDockService.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/Services/IAvalonDockService.cs
@@ -1,11 +1,17 @@
 ﻿using LogXtreme.WinDsk.Infrastructure.Events;
 using System;
+using System.Collections.Generic;
 
 namespace LogXtreme.WinDsk.Infrastructure.Services {
+
     public interface IAvalonDockService {
 
         event EventHandler<AvalonDockEventArgs> DockingManagerChanged;
 
         void RaiseDockingManagerChanged(object sender, AvalonDockEventArgs args);
+
+        IDictionary<string, Type> RegisteredParts { get; }
+
+        void RegisterPart<T>(string partName);
     }
 }
--- Apps/WinDesktop/Modules/AppServices/AvalonDockService/AvalonDockService.cs
+++ Apps/WinDesktop/Modules/AppServices/AvalonDockService/AvalonDockService.cs
@@ -1,10 +1,17 @@
 ﻿using LogXtreme.WinDsk.Infrastructure.Events;
 using LogXtreme.WinDsk.Infrastructure.Services;
 using System;
+using System.Collections.Generic;
 
 namespace LogXtreme.WinDsk.Modules.Services {
 
-    public class AvalonDockService : IAvalonDockService {            
+    public class AvalonDockService : IAvalonDockService {
+
+        private Dictionary<string, Type> registeredParts =
+            new Dictionary<string, Type>();
+
+        public IDictionary<string, Type> RegisteredParts =>
+            this.registeredParts;
 
         public event EventHandler<AvalonDockEventArgs> DockingManagerChanged;
 
@@ -13,6 +20,15 @@ namespace LogXtreme.WinDsk.Modules.Services {
             AvalonDockEventArgs args) {
 
             this.DockingManagerChanged?.Invoke(sender, args);
+        }
+
+        public void RegisterPart<T>(string partName) {
+
+            if (this.registeredParts.ContainsKey(partName)) {
+                return;
+            }
+
+            this.registeredParts.Add(partName, typeof(T));
         }
     }
 }
