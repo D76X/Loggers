dc93b55c77085d9ed72bf4727cd888bbf30d08a2
Davide Spano <davide.spano.x@gmail.com>
2018-04-07 11:48:50 +0100

    chnaged RefionNames.cs in infrastructure the RegionDockingManager now replaces the left right and centre regions used before DockingManager was used

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -7,14 +7,8 @@ namespace LogXtreme.WinDsk.Infrastructure {
         public const string RegionToolbar = @"RegionToolbar";
 
         public const string RegionContent = @"RegionContent";        
-        public const string RegionLeftPanels = @"RegionLeftPanels";
-        public const string RegionWorkspace = @"RegionWorkspace";
-        public const string RegionRightPanels = @"RegionRightPanels";
 
         public const string RegionDockingManager = @"RegionDockingManager";
-        public const string RegionDocumentPane = @"RegionDocumentPane";
-        public const string RegionDataTree = @"RegionDataTree";
-        public const string RegionDeviceTree = @"RegionDeviceTree";
         public const string RegionStatusBar = @"RegionStatusBar";        
     }
 }
