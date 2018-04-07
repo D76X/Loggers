ad7213d31e61352ffbfe20e74796f3e1a97f1a9c
Davide Spano <davide.spano.x@gmail.com>
2018-04-07 11:49:25 +0100

    spacing change?

--- Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
+++ Apps/WinDesktop/LogXtreme.WinDsk.Infrastructure/RegionNames.cs
@@ -5,9 +5,7 @@ namespace LogXtreme.WinDsk.Infrastructure {
 
         public const string RegionMainMenu = @"RegionMainMenu";
         public const string RegionToolbar = @"RegionToolbar";
-
-        public const string RegionContent = @"RegionContent";        
-
+        public const string RegionContent = @"RegionContent";
         public const string RegionDockingManager = @"RegionDockingManager";
         public const string RegionStatusBar = @"RegionStatusBar";        
     }
