b7a0f14ea42b4872b1e78610c8abfc0fc8fed264
Davide Spano <davide.spano.x@gmail.com>
2018-04-13 13:21:47 +0100

    improved FlushOnTriggerTest

--- Apps/Extensions/LogXtreme.Reactive.Extensions.Test.1/FlushOnTriggerTest.cs
+++ Apps/Extensions/LogXtreme.Reactive.Extensions.Test.1/FlushOnTriggerTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 /// <summary>
@@ -11,6 +13,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 /// Subjects
 /// http://www.introtorx.com/Content/v1.0.10621.0/02_KeyTypes.html#Subject
 /// http://reactivex.io/documentation/subject.html
+/// Observable.Create
+/// http://www.introtorx.com/content/v1.0.10621.0/04_CreatingObservableSequences.html#ObservableCreate
 /// </summary>
 namespace LogXtreme.Reactive.Extensions.Test._1 {
 
@@ -20,24 +24,79 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
         [TestMethod]
         public void TestMethod1() {
 
-            int leftBound = 0;
-            int rightBound = 10;
-            int midPoint = 5;
-            int bufferSize = 11;
+            // arrange
+            int threshold = 3;
+            int bufferSize = threshold+3;
+            var receivedValuesFromUnbufferedSubscription = new List<int>();
+            var receivedValuesFromBufferedSubscription = new List<int>();
 
-            IObserver<int> observer = 
+            // the observable is cold until it is subscribed to
+            //IObservable<int> observableRange = 
+            //    Observable.Range(leftBound, rightBound);
 
-            IObservable<int> range = 
-                Observable.Range(leftBound, rightBound);
+            //var subject 
+            var subject = new Subject<int>();
+            IObservable<int> observable = subject;
 
             Func<int, bool> triggerPredicate = i => {
-                return i > midPoint;
+                return i > threshold;
             };
 
-            IObservable<int> bufferedRange = 
-                range.FlushOnTrigger<int>(triggerPredicate, bufferSize);
+            IObservable<int> bufferedObservable = 
+                observable.FlushOnTrigger<int>(triggerPredicate, bufferSize);
 
-            var subcriber = bufferedRange.Subscribe()
+            var subcriberToUnbuffered = observable
+                .Subscribe(i => receivedValuesFromUnbufferedSubscription.Add(i));
+
+            // by subscribing to the observable it becomes hot
+            var subcriberToBuffered = bufferedObservable
+                .Subscribe(i => receivedValuesFromBufferedSubscription.Add(i));
+
+            // act
+            // do nothing!
+
+            // assert - if threshold fails the test below must be adjusted
+            Assert.AreEqual(3, threshold);
+
+            // assert
+            Assert.AreEqual(0, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(1);
+
+            // assert
+            Assert.AreEqual(1, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(2);
+
+            // assert
+            Assert.AreEqual(2, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(3);
+
+            // assert
+            Assert.AreEqual(3, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act - this is the first value after the threshold 
+            subject.OnNext(4);
+
+            // assert            
+            Assert.AreEqual(4, receivedValuesFromUnbufferedSubscription.Count);
+            // the bufferred values are flushed to the subscriber
+            Assert.AreEqual(4, receivedValuesFromBufferedSubscription.Count);
+
+            // act - this is the first value after the threshold 
+            subject.OnNext(5);
+
+            // assert - now buffered and unbuffed should stay in sync
+            Assert.AreEqual(5, receivedValuesFromUnbufferedSubscription.Count);            
+            Assert.AreEqual(5, receivedValuesFromBufferedSubscription.Count);
         }
     }
 }
