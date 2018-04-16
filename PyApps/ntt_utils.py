1f8236ccb44414f0064056060f7381d93d6f4576
Davide Spano <davide.spano.x@gmail.com>
2018-04-16 18:36:04 +0100

    chnages to PyApps+

--- PyApps/ntt_utils.py
+++ PyApps/ntt_utils.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-    This modules defines some utilities functions. 
+    This modules defines some utilities functions.
 
     Usage:
 
     # Copy and paste all these commands in the terminal to see the outputs.
-    import os; os.chdir("C:\\GitHub\\Loggers\\PyApps"); clear = lambda: os.system('cls'); import imp; import ntt_utils as nttutils; nttutils.test_module()
+    import os; os.chdir("C:\\GitHub\\Loggers\\PyApps"); clear = lambda: os.system(
+        'cls'); import imp; import ntt_utils as nttutils; nttutils.test_module()
 
     # The last two commands are specific to this module.
     import ntt_utils as nttutils
@@ -16,17 +17,18 @@
     imp.reload(nttutils)
 
     # clear th REPL
-    clear = lambda: os.system('cls')  
-    clear() 
+    clear = lambda: os.system('cls')
+    clear()
 
     # set the working directory in the REPL
     import os
     os.chdir("C:\\GitHub\\Loggers\\PyApps")
     os.getcwd()
 
-"""   
+"""
 
 from math import sqrt
+
 
 def sign(x):
     """
@@ -34,21 +36,22 @@ def sign(x):
 
     Args:
         x: a numeric value.
-    
+
     Returns:
         +1 for positive values
         -1 for negative values
          0 for zero
-    
-    Notes: 
-        This function is a clever one-liner that exploits that fact that in Python 
-        the following identities hold. 
+
+    Notes:
+        This function is a clever one-liner that exploits that fact that in Python
+        the following identities hold.
         True  - True  = 0
         True  - False = 1
         False - True  = -1
-        False - False = 0  
+        False - False = 0
     """
     return (x > 0) - (x < 0)
+
 
 def isPrime(n):
     """
@@ -62,13 +65,14 @@ def isPrime(n):
             return False
     return True
 
+
 def distinct(iterable):
     """
     Eliminates duplicates that may be present in 'iterable'.
 
     Args:
         iterable: the source series.
-    
+
     Yields:
         an iterable with unique elements from 'iterable'.
     """
@@ -79,23 +83,65 @@ def distinct(iterable):
         yield item
         seen.add(item)
 
-def 
 
-def test_module(): 
+def count_words(text):
+    """
+    Counts the occurrences of words in a piece of text.
+
+    Args:
+        text: the text.
+
+    Returns:
+        a dictionary of string vs number.
+
+    Notes:
+        Case is ignored.
+    """
+    normalised_text = ''.join(c.lower() if c.isalpha() else ' ' for c in text)
+    frequencies = {}
+    for world in normalised_text.split():
+        frequencies[world] = frequencies.get(world, 0) + 1
+    return frequencies
+
+
+def test_module():
     """Module-level tests."""
-    print()    
+
+    print()
     print("ntt_utils tests...")
+
+    print()
+    print("test sign(x)")
     print("sign(1)={}".format(sign(1)))
     print("sign(1.6)={}".format(sign(1.06)))
     print("sign(-1)={}".format(sign(-1)))
     print("sign(-1.01)={}".format(sign(-1.01)))
-    print("sign(0)={}".format(sign(0)))    
+    print("sign(0)={}".format(sign(0)))
+
+    print()
+    print("isPrime(n)"
+    print("isPrime(1) = {}".forma(isPrime(1)))
+    print("isPrime(2) = {}".forma(isPrime(2)))
+    print("isPrime(3) = {}".forma(isPrime(3)))
+    print("isPrime(4) = {}".forma(isPrime(4)))
+    print("isPrime(5) = {}".forma(isPrime(5)))
+    print("isPrime(6) = {}".forma(isPrime(6)))
+
+    print()
+    print("distinct(iterable))
+    print("distinct([1, 1, 2, 2, 3, 3]) = {}".format(distinct([1, 1, 2, 2, 3, 3])))
+
+    print()
+    print("count_words(text)")
+    text = "There is some text to test the count or words with - is the count of words right?"
+    print("text = {}".format(text))
+    print("{}".format(count_words(text)))
 
 # ##########################################################################################
 
 # 1-Run the file in Python as a script.
 
-# if __name__ = '__main__' the files is executed as a script 
+# if __name__ = '__main__' the files is executed as a script
 # to execute the file as a scrip in the cmd use : python filename.py
 
 # 2-Import the module into the Python REPL.
