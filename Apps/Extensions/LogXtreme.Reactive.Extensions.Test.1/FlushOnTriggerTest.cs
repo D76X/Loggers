b1abb92484c3477463af0a59399ee231028e9a18
Davide Spano <davide.spano.x@gmail.com>
2018-04-18 15:01:43 +0100

    added test EventsAreQueuedUntilThresholdIsReachedAndThenRepeatTheSameBehaviourAfterTheBufferIsFlushed

--- Apps/Extensions/LogXtreme.Reactive.Extensions.Test.1/FlushOnTriggerTest.cs
+++ Apps/Extensions/LogXtreme.Reactive.Extensions.Test.1/FlushOnTriggerTest.cs
@@ -43,7 +43,6 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
             var subcriberToUnbuffered = observable
                 .Subscribe(i => receivedValuesFromUnbufferedSubscription.Add(i));
 
-            // by subscribing to the observable it becomes hot
             var subcriberToBuffered = bufferedObservable
                 .Subscribe(i => receivedValuesFromBufferedSubscription.Add(i));
 
@@ -92,6 +91,104 @@ namespace LogXtreme.Reactive.Extensions.Test._1 {
             // assert - now buffered and unbuffed should stay in sync
             Assert.AreEqual(5, receivedValuesFromUnbufferedSubscription.Count);            
             Assert.AreEqual(5, receivedValuesFromBufferedSubscription.Count);
+        }
+
+        [TestMethod]
+        public void EventsAreQueuedUntilThresholdIsReachedAndThenRepeatTheSameBehaviourAfterTheBufferIsFlushed() {
+
+            // arrange
+            int threshold = 3;
+            int bufferSize = threshold + 3;
+            var receivedValuesFromUnbufferedSubscription = new List<int>();
+            var receivedValuesFromBufferedSubscription = new List<int>();
+
+            var subject = new Subject<int>();
+            IObservable<int> observable = subject;
+
+            Func<int, bool> triggerPredicate = i => {
+                return i > threshold;
+            };
+
+            IObservable<int> bufferedObservable =
+                observable.FlushOnTrigger<int>(triggerPredicate, bufferSize);
+
+            var subcriberToUnbuffered = observable
+                .Subscribe(i => receivedValuesFromUnbufferedSubscription.Add(i));
+
+            var subcriberToBuffered = bufferedObservable
+                .Subscribe(i => receivedValuesFromBufferedSubscription.Add(i));
+
+            // act
+            // do nothing!
+
+            // assert - if threshold fails the test below must be adjusted
+            Assert.AreEqual(3, threshold);
+
+            // assert
+            Assert.AreEqual(0, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(1);
+
+            // assert
+            Assert.AreEqual(1, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(2);
+
+            // assert
+            Assert.AreEqual(2, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(3);
+
+            // assert
+            Assert.AreEqual(3, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(0, receivedValuesFromBufferedSubscription.Count);
+
+            // act - this is the first value above the threshold 
+            subject.OnNext(4);
+
+            // assert            
+            Assert.AreEqual(4, receivedValuesFromUnbufferedSubscription.Count);
+            // the bufferred values are flushed to the subscriber
+            Assert.AreEqual(4, receivedValuesFromBufferedSubscription.Count);
+
+            // now we repeat the sequence from the begining and we expect the 
+            // same behavior where four items are flushed to the observer when
+            // the threshold is passed again.            
+
+            // act
+            subject.OnNext(1);
+
+            // assert
+            Assert.AreEqual(5, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(4, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(2);
+
+            // assert
+            Assert.AreEqual(6, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(4, receivedValuesFromBufferedSubscription.Count);
+
+            // act
+            subject.OnNext(3);
+
+            // assert
+            Assert.AreEqual(7, receivedValuesFromUnbufferedSubscription.Count);
+            Assert.AreEqual(4, receivedValuesFromBufferedSubscription.Count);
+
+            // act - this causes to thread the threshold a second time
+            subject.OnNext(4);
+
+            // assert            
+            Assert.AreEqual(8, receivedValuesFromUnbufferedSubscription.Count);
+            // the bufferred values are flushed to the subscriber
+            Assert.AreEqual(8, receivedValuesFromBufferedSubscription.Count);
         }
     }
 }
