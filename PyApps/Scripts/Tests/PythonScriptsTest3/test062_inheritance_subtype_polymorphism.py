3bd1eaaecf9dbf04531236af91f4b36512986219
Davide Spano <davide.spano.x@gmail.com>
2018-04-25 17:07:17 +0100

    changed to PyApps

--- PyApps/Scripts/Tests/PythonScriptsTest3/test062_inheritance_subtype_polymorphism.py
+++ PyApps/Scripts/Tests/PythonScriptsTest3/test062_inheritance_subtype_polymorphism.py
@@ -1,12 +1,13 @@
 
 #!/usr/bin/env python3
 """
-This module illustrates how to implement the iterable protocol in Python.
+This module illustrates of single and multiple inheritance and polymorphism in Python.
 
 Usage:
 
     # Copy and paste all these commands in the terminal to see the outputs.
-    import os; os.chdir("C:\\GitHub\\Loggers\\PyApps\\Scripts\\Tests\\PythonScriptsTest3"); clear = lambda: os.system('cls'); import imp; import test062_inheritance_subtype_polymorphism as t62; t62.test_module()
+    import os; os.chdir("C:\\GitHub\\Loggers\\PyApps\\Scripts\\Tests\\PythonScriptsTest3"); clear = lambda: os.system(
+        'cls'); import imp; import test062_inheritance_subtype_polymorphism as t62; t62.test_module()
 
     # The last two commands are specific to this module.
     import test062_inheritance_subtype_polymorphism as t62
@@ -117,6 +118,46 @@ class SortedList(SimpleList):
 
 # ################################################################################################
 
+# The basics of multiple inheritance
+
+
+class IntList(SimpleList):
+    """
+    A list that can only take integers.
+    """
+
+    def __init__(self, items=()):
+        for x in items:
+            self._validate(x)
+        super().__init__(items)
+
+    @staticmethod
+    def _validate(x):
+        if not isinstance(x, int):
+            raise TypeError("IntList only supports integer values.")
+
+    def add(self, item):
+        self._validate(item)
+        super().add(item)
+
+    def __repr__(self):
+        return "IntList({!r})".format(list(self))
+
+ # this class inherits from multiple base classes!
+ # it leaves open some questions
+ # 1- both base classes define the add method - which one is called on the descendant?
+
+
+class SortedIntList(IntList, SortedList):
+    """
+    A sorted list of integers.
+    """
+
+    def __repr__(self):
+        return "SortedIntList({!r})".format(list(self))
+
+# ################################################################################################
+
 
 def test_module():
     """Module-level tests."""
@@ -150,6 +191,64 @@ def test_module():
     sortedList.add(-8)
     print("sortedList.add(-8)...")
     print(sortedList)
+
+    # Type checking in Python
+    print()
+    print("Type checking in Python")
+
+    # isinstance()
+
+    print()
+    print("use isinstance() ...to check the type of an istance")
+    print("isinstance(3, int) = {}".format(isinstance(3, int)))
+    print("isinstance(3, str) = {}".format(isinstance(3, str)))
+    print("isinstance(""hello"", str) = {}".format(isinstance("hello", str)))
+    print("isinstance(""hello"", int) = {}".format(isinstance("hello", int)))
+    print("isinstance(sortedList, SortedList) = {}".format(
+        isinstance(sortedList, SortedList)))
+    print("isinstance(sortedList, SimpleList) = {}".format(
+        isinstance(sortedList, SimpleList)))
+
+    print()
+    print("isinstance can be use with a tuple as its second argument!")
+    print("isinstance(3, (int, str, SimpleList)) = {}".format(
+        isinstance(3, (int, str, SimpleList))))
+    print("isinstance(3, (list, str, SimpleList)) = {}".format(
+        isinstance(3, (list, str, SimpleList))))
+
+    print()
+    intList = IntList([1, 2, 3, 4])
+    print("intList = IntList([1, 2, 3, 4]) = {}".format(intList))
+    intList.add(10)
+    print(
+        "intList.add(10) => intList = IntList([1, 2, 3, 4]) = {}".format(intList))
+
+    # issubclass()
+    print()
+    print("use issubclass()...to check that a type is a subtype of some other type")
+    print("issubclass() looks at the entire inheritance graph nt only the direct parent of the type used as its first arg")
+    print("issubclass(IntList, SimpleList) = {}".format(
+        issubclass(IntList, SimpleList)))
+    print("issubclass(IntList, SortedList) = {}".format(
+        issubclass(IntList, SortedList)))
+
+    # multiple inheritance
+    print("multiple inheritance")
+    sortedIntList = SortedIntList([3, 5, 4, 19, 6])
+    print("sortedIntList = SortedIntList([3,5, 4, 19, 6]) = {}".format(
+        sortedIntList))
+    sortedIntList.add(-100)
+    print("sortedIntList.add(-100) => sortedList = {}".format(sortedIntList))
+    print("isinstance(sortedIntList, IntList) = {}".format(
+        isinstance(sortedIntList, IntList)))
+    print("isinstance(sortedIntList, sortedList) = {}".format(
+        isinstance(sortedIntList, SortedList)))
+    print("issubclass(SortedIntList, SimpleList) = {}".format(
+        issubclass(SortedIntList, SimpleList)))
+    print("issubclass(SortedIntList, IntList) = {}".format(
+        issubclass(SortedIntList, IntList)))
+    print("issubclass(SortedIntList, SortedList) = {}".format(
+        issubclass(SortedIntList, SortedList)))
 
 # ##########################################################################################
 
