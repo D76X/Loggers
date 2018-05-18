250bbdc55104aa813ae18d6b0bf829436eabf378
Davide Spano <davide.spano.x@gmail.com>
2018-05-18 15:23:35 +0100

    changes to Demo.03 ZEROMQ vntilator pattern

--- Examples/ZeroMQ-Examples/Demo.03.ZeroMQ.Sink.PULL/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.03.ZeroMQ.Sink.PULL/Program.cs
@@ -7,15 +7,18 @@ namespace Demo._03.ZeroMQ.Sink.PULL {
     class Program {
 
 
-        // The sink node collects the results form the worker nodes.
-        // It also receices a start message from the ventilator node.
-        // It binds a single PULL socket to an IP address to receive all messages. 
+        // The sink node. 
+        // It binds a single PULL socket to an IP address to receive all messages.
+        // It receices a start message for the batch from the ventilator node.
+        // It receices a message with the total workload for the batch from the ventilator node.
+        // It receices a message with the batch sixe for the batch from the ventilator node.
+        // It collects the results from the worker nodes for the bacth and compiles some stats.
 
         static void Main(string[] args) {           
 
             // ZMQ Context and client socket
             using (ZmqContext context = ZmqContext.Create())
-            using (ZmqSocket sink = context.CreateSocket(SocketType.PUSH)) {
+            using (ZmqSocket sink = context.CreateSocket(SocketType.PULL)) {
 
                 // the sink socket is a stable endpoint thus it binds to its IP address.                
                 sink.Bind("tcp://*:5558");
@@ -23,14 +26,21 @@ namespace Demo._03.ZeroMQ.Sink.PULL {
                 // the sink must wait for a start signal before doing anything else.
                 // the start signal comes from the ventilator once it has handed out the work to the worker nodes.
                 // the call to receive frame blocks.
-                sink.ReceiveFrame();
+                Frame frame = sink.ReceiveFrame();
+
+                // the sink receives the expected workload for this batch
+                frame = sink.ReceiveFrame();
+
+                // the sink receives the expected batch size for this batch
+                frame = sink.ReceiveFrame();
+
+                int batchSize = 100;
 
                 var stopwatch = new Stopwatch();
                 stopwatch.Start();
 
-                for (int i = 0; i < 100; i++) {
+                for (int i = 0; i < batchSize; i++) {
 
-                    // blocking receive
                     sink.ReceiveFrame();
 
                     // do something...
--- Examples/ZeroMQ-Examples/Demo.03.ZeroMQ.Ventilator.PUSH/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.03.ZeroMQ.Ventilator.PUSH/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using ZeroMQ;
 
 namespace Demo._03.ZeroMQ.Ventilator.PUSH {
@@ -7,39 +8,56 @@ namespace Demo._03.ZeroMQ.Ventilator.PUSH {
 
             // The ventilator produces tasks that can be done in parallel.
             // The ventilator uses two sockets in its implementation.
-            // It binds a PUSH socket to send a message to any available worker device node.
+            // It binds a PUSH socket to send messages to any available worker device nodes.
             // It connects a PUSH socket to send a start message to the sink.
+            // It load balances the work amongst all connected workers.
 
             // ZMQ Context and client socket
             using (ZmqContext context = ZmqContext.Create())
             using (ZmqSocket sender = context.CreateSocket(SocketType.PUSH))
             using (ZmqSocket sink = context.CreateSocket(SocketType.PUSH)) {
 
-                // the sender socket is a stable endpoint.
-                // despite being a PUSH socket it can bind to an IP address.
+                // the sender socket is a stable endpoint thus it binds to an IP address.
                 sender.Bind("tcp://*:5557");
 
-                // the sink socket
+                // connect to the sink socket
                 sink.Connect("tcp://localhost:5558");
 
-                // send a start message to the sink...
+                Console.WriteLine("Sever Load Balancing...");
+                Console.WriteLine("Wait a bit before sending out any work.");
+                Console.WriteLine("Allow time to connect to the sink.");
+                Console.WriteLine("Allow tile to let the available workers to establish a connection.");
+                Console.WriteLine();
+
+                Console.WriteLine("The server is ready to send!");
+                Thread.Sleep(2000);
+
+                Console.WriteLine("send a start message to the sink...");
                 sink.Send(new byte[] { 0x00 }, 1, SocketFlags.DontWait);
 
+                Console.WriteLine("split the work, create a batch...");
+                int batchSize = 100; 
                 var rnd = new Random();
-
+                int[] workLoad = new int[batchSize];
                 long totalWorkLoad = 0;
 
-                for (int i = 0; i < 100; i++) {
-
-                    int workLoad = rnd.Next(101);
-                    totalWorkLoad += workLoad;
-                    byte[] action = BitConverter.GetBytes(workLoad);
-
-                    sender.Send(action, action.Length, SocketFlags.DontWait);
+                for (int i = 0; i < batchSize; i++) {
+                    workLoad[i] = rnd.Next(101);
+                    totalWorkLoad += workLoad[i];
                 }
 
-                // prepare the message for the workes 
+                Console.WriteLine("send the expected workload message for this batch to the sink...");
+                sink.Send(BitConverter.GetBytes(totalWorkLoad), 1, SocketFlags.DontWait);
 
+                // hand out the work
+                for (int i = 0; i < batchSize; i++) {    
+                    
+                    byte[] work = BitConverter.GetBytes(workLoad[i]);
+                    sender.Send(work, work.Length, SocketFlags.DontWait);
+                    Console.WriteLine($"sent work {workLoad[i]}");
+                }
+
+                Console.WriteLine($"sent all work {totalWorkLoad}");
             }
         }
     }
