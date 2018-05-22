c02786e9c3cc4c7be69ce4520e40fed53d9f22f6
Davide Spano <davide.spano.x@gmail.com>
2018-05-22 18:15:27 +0100

    changes to Demo.04 PUB-SUB

--- Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Publisher1.PUB/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Publisher1.PUB/Program.cs
@@ -12,38 +12,56 @@ namespace Demo._04.ZeroMQ.Publisher1.PUB {
             }
 
             using (ZmqContext context = ZmqContext.Create())
-            using (ZmqSocket publisher_1_1 = context.CreateSocket(SocketType.PUB))
-            using (ZmqSocket publisher_1_2 = context.CreateSocket(SocketType.PUB)) {
+            using (ZmqSocket publisherArea1a = context.CreateSocket(SocketType.PUB))
+            using (ZmqSocket publisherArea1b = context.CreateSocket(SocketType.PUB)) {
 
-                string address_1_1 = "tcp://*:5556";
-                publisher_1_1.Bind(address_1_1);
-                Console.WriteLine("Publisher_1_1 bound on to {0}", address_1_1);
+                string endPointArea1a = "tcp://*:5556";
+                publisherArea1a.Bind(endPointArea1a);
+                Console.WriteLine("Publisher for Area 1a bound on to {0}", endPointArea1a);
 
-                string address_1_2 = "tcp://*:5557";
-                publisher_1_2.Bind(address_1_2);
-                Console.WriteLine("Publisher_1_2 bound on to {0}", address_1_2);
+                string endPoitArea1b = "tcp://*:5557";
+                publisherArea1b.Bind(endPoitArea1b);
+                Console.WriteLine("Publisher for Area 1b bound on to {0}", endPoitArea1b);
+                Console.WriteLine();
 
                 var rnd = new Random();
+                Frame keyArea1b = new Frame(Encoding.UTF8.GetBytes("READINGS_AREA_1B"));
 
                 while (true) {
 
-                    // This is way of sending a whole message as a single encoded string.
-                    // The first token of the message can be used by subscribers to 
-                    // selctively subscribe to spevific messages. The token is negotiated 
-                    // at the time the subscription is requested by the SUB socket. The
-                    // PUB socket will only send those messages to the SUB socket that it
-                    // has originally subscribed to. This keeps network traffic to a minimum.
+                    // In some cases individial messages are sent out by the PUB.
+                    // The SUB end point can still subscribe to only those messages that are of interest to 
+                    // them by specifying the prefix at the time of subscription to the publisher.
+                    // The messages are filtered out at the PUB socket end which keeps the network traffic 
+                    // to a minimum.
+                    // The Send method is way of sending a whole message as a single encoded string.
+                    // The Sned method is asynchronous.
+                    var stArea1a = DateTime.Now;
+                    var tArea1a = rnd.Next(301);
+                    var pArea1a = rnd.Next(11);
+                    publisherArea1a.Send($"SAMPLE_TIME_AREA_1A {stArea1a}", Encoding.Unicode);
+                    publisherArea1a.Send($"TEMPERATURE_AREA_1A {tArea1a}", Encoding.Unicode);
+                    publisherArea1a.Send($"PRESSURE_AREA_1A {pArea1a}", Encoding.Unicode);
+                    Console.WriteLine($"Area 1A readings t = {tArea1a}, p = {pArea1a} @ {stArea1a}");
+                    //Console.WriteLine();    
+                   
+                    // Messages can also be Multipart that is composed by more than one frame.
+                    // In PUB-SUB patterns it is typical to keep the key and the data split in
+                    // different frames. The first frame of a multipart message can be used as
+                    // a subscription key by the subscriber.
+                    // The key frame is also known as the Message envelope.
+                    var stArea1b = DateTime.Now;
+                    var tArea1b = rnd.Next(301);
+                    var pArea1b = rnd.Next(11);
+                    var message = new ZmqMessage();               
+                    message.Append(keyArea1b);
+                    message.Append(new Frame(BitConverter.GetBytes(stArea1b.Ticks)));
+                    message.Append(new Frame(BitConverter.GetBytes(tArea1b)));
+                    message.Append(new Frame(BitConverter.GetBytes(pArea1b)));
+                    publisherArea1a.SendMessage(message);
+                    Console.WriteLine($"Area 1B readings t = {tArea1a}, p = {pArea1a} @ {stArea1a}");
 
-                    publisher_1_1.Send($"TEMPERATURE {rnd.Next(300)}", Encoding.Unicode);
-
-                    // multipart messages can also be composed into a frame.
-                    // In PUB-SUB patterns it is typical to have the key and the data split 
-                    // into different frames. The key frame is aka Message envelope.
-
-                    var message = new ZmqMessage();
-                    Frame key = new Frame(BitConverter.GetBytes())
-                    message.Append()
-                    publisher_1_2.SendMessage()
+                    Thread.Sleep(1000);
                 }
             }
         }
--- Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Subscriber.SUB/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Subscriber.SUB/Program.cs
@@ -1,10 +1,60 @@
 ﻿using System;
-using System.Threading;
+using System.Linq;
+using System.Text;
 using ZeroMQ;
 
 namespace Demo._04.ZeroMQ.Subscriber.SUB {
     class Program {
         static void Main(string[] args) {
+
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine($"{args[i]}");
+            }
+
+            using (ZmqContext context = ZmqContext.Create())
+            using (ZmqSocket subscriber = context.CreateSocket(SocketType.SUB)) {
+
+                string endPointArea1a = "tcp://localhost:5556";
+                string endPointArea1b = "tcp://localhost:5557";
+
+                subscriber.Connect(endPointArea1a);
+                subscriber.Connect(endPointArea1b);
+                Console.WriteLine($"Connect to Area 1A monitor {endPointArea1a}");
+                Console.WriteLine($"Connect to Area 1B monitor {endPointArea1a}");
+                Console.WriteLine();
+
+                subscriber.SubscribeAll();
+                Console.WriteLine("Subcribed to all messages for Area 1");
+                Console.WriteLine();
+
+                while (true) {
+
+                    // readings from Area1a
+                    // these calls are all blocking
+                    var stArea1a = subscriber.Receive(Encoding.Unicode);
+                    Console.WriteLine($"received sample time area 1a {stArea1a}");
+                    var tArea1a = subscriber.Receive(Encoding.Unicode);
+                    Console.WriteLine($"received temperature reading area 1a {tArea1a}");
+                    var pArea1a = subscriber.Receive(Encoding.Unicode);
+                    Console.WriteLine($"received pressure reading area 1a {stArea1a}");
+
+                    Console.WriteLine($"Area 1A readings t = {tArea1a.Split()[1]}, p = {pArea1a.Split()[1]} @ {stArea1a.Split()[1]}");
+                    Console.WriteLine();
+
+                    // readings from Area1b
+                    ZmqMessage rArea1bMsg = subscriber.ReceiveMessage();
+                    int nFrames = rArea1bMsg.FrameCount;
+                    Console.WriteLine($"Area1b readings 1 message with {nFrames} frames");
+                    Frame[] frames = rArea1bMsg.ToArray();
+                    string keyArea1b = Encoding.UTF8.GetString(frames[0].Buffer);
+                    DateTime stArea1b = new DateTime(BitConverter.ToInt64(frames[1].Buffer, 0));
+                    int tArea1b = BitConverter.ToInt32(frames[2].Buffer,0);
+                    int pArea1b = BitConverter.ToInt32(frames[3].Buffer, 0);
+                    Console.WriteLine($"Area 1B readings t = {tArea1b}, p = {pArea1b} @ {stArea1b}");
+
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
