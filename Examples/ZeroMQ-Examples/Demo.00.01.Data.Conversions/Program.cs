acf92ac51aa6cacaa4c04791afefb021d669c5c8
Davide Spano <davide.spano.x@gmail.com>
2018-05-23 12:48:24 +0100

    chnages to ZeroMQ eamples for PUB-SUB demo

---
+++ Examples/ZeroMQ-Examples/Demo.00.01.Data.Conversions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using ZeroMQ;
+
+namespace Demo._00._01.Data.Conversions {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // strings as UTF8 
+            Console.WriteLine("UTF8");
+            string testUtf8 = @"Test";
+            byte[] testUtf8Bytes = Encoding.UTF8.GetBytes(testUtf8);
+            string testUtf8String = Encoding.UTF8.GetString(testUtf8Bytes);
+            string testUtf8BytesString = BitConverter.ToString(testUtf8Bytes);
+            Console.WriteLine($"{testUtf8} as bytes = {testUtf8BytesString}");
+            Console.WriteLine($"{testUtf8BytesString} as string = {testUtf8String}");
+            Console.WriteLine();
+
+            // unicode strings...
+            Console.WriteLine("Unicode");
+            string testUnicode = @"Test⅑←";
+            byte[] testUnicodeBytes = Encoding.Unicode.GetBytes(testUnicode);
+            string testUnicodeString = Encoding.Unicode.GetString(testUnicodeBytes);
+            string testUnicodeBytesString = BitConverter.ToString(testUnicodeBytes);
+            Console.WriteLine($"{testUnicode} as bytes = {testUnicodeBytesString}");
+            Console.WriteLine($"{testUnicodeBytesString} as string = {testUnicodeString}");
+            Console.WriteLine();
+
+            // DateTime
+            Console.WriteLine("DateTime");
+            DateTime testDateTime = DateTime.Now;
+            byte[] testDateTimeTicksBytes = BitConverter.GetBytes(testDateTime.Ticks);
+            string testDateTimeTicksBytesString = BitConverter.ToString(testDateTimeTicksBytes);
+            DateTime testDateTimeBack = DateTime.FromBinary(BitConverter.ToInt64(testDateTimeTicksBytes,0));
+            Console.WriteLine($"{testDateTime} ticks as bytes  = {testDateTimeTicksBytesString}");
+            Console.WriteLine($"{testDateTimeTicksBytesString} as string = {testDateTimeBack}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Subscriber.SUB/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Subscriber.SUB/Program.cs
@@ -4,7 +4,9 @@ using System.Text;
 using ZeroMQ;
 
 namespace Demo._04.ZeroMQ.Subscriber.SUB {
+
     class Program {
+
         static void Main(string[] args) {
 
             for (int i = 0; i < args.Length; i++) {
@@ -31,25 +33,27 @@ namespace Demo._04.ZeroMQ.Subscriber.SUB {
 
                     // readings from Area1a
                     // these calls are all blocking
-                    var stArea1a = subscriber.Receive(Encoding.Unicode);
+                    string stArea1a = subscriber.Receive(Encoding.Unicode);
+                    string tArea1a = subscriber.Receive(Encoding.Unicode);
+                    string pArea1a = subscriber.Receive(Encoding.Unicode);
+                    string stArea1aValue = stArea1a.Substring(stArea1a.IndexOf(' ')+1);
+                    string tArea1aValue = tArea1a.Substring(tArea1a.IndexOf(' ')+1);
+                    string ptArea1aValue = pArea1a.Substring(pArea1a.IndexOf(' ')+1);
                     Console.WriteLine($"received sample time area 1a {stArea1a}");
-                    var tArea1a = subscriber.Receive(Encoding.Unicode);
                     Console.WriteLine($"received temperature reading area 1a {tArea1a}");
-                    var pArea1a = subscriber.Receive(Encoding.Unicode);
                     Console.WriteLine($"received pressure reading area 1a {stArea1a}");
-
-                    Console.WriteLine($"Area 1A readings t = {tArea1a.Split()[1]}, p = {pArea1a.Split()[1]} @ {stArea1a.Split()[1]}");
+                    Console.WriteLine($"Area 1A readings t = {tArea1aValue}, p = {ptArea1aValue} @ {stArea1aValue}");
                     Console.WriteLine();
 
                     // readings from Area1b
                     ZmqMessage rArea1bMsg = subscriber.ReceiveMessage();
-                    int nFrames = rArea1bMsg.FrameCount;
-                    Console.WriteLine($"Area1b readings 1 message with {nFrames} frames");
+                    int nFrames = rArea1bMsg.FrameCount;                    
                     Frame[] frames = rArea1bMsg.ToArray();
                     string keyArea1b = Encoding.UTF8.GetString(frames[0].Buffer);
-                    DateTime stArea1b = new DateTime(BitConverter.ToInt64(frames[1].Buffer, 0));
+                    DateTime stArea1b = DateTime.FromBinary(BitConverter.ToInt64(frames[1].Buffer, 0));
                     int tArea1b = BitConverter.ToInt32(frames[2].Buffer,0);
                     int pArea1b = BitConverter.ToInt32(frames[3].Buffer, 0);
+                    Console.WriteLine($"Area1b readings 1 message with {nFrames} frames");
                     Console.WriteLine($"Area 1B readings t = {tArea1b}, p = {pArea1b} @ {stArea1b}");
 
                     Console.WriteLine();
