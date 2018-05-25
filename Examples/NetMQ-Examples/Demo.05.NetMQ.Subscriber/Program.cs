7482d6c70e2f051c3bcd4b67c27a42f4f35b8407
Davide Spano <davide.spano.x@gmail.com>
2018-05-25 16:22:56 +0100

    added Demo.05 XSUB.XPUB for ZeroMQ and NetMQ with corresponding NuGet packages and references.

---
+++ Examples/NetMQ-Examples/Demo.05.NetMQ.Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo._05.NetMQ.Subscriber {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
