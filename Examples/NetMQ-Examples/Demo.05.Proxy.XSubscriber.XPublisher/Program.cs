e30173a01c90860b9e99943b673085bdb6928ec5
Davide Spano <davide.spano.x@gmail.com>
2018-05-26 14:50:12 +0100

    added proxy XSUB-XPUB NetMQ for Demo.05

--- Examples/NetMQ-Examples/Demo.05.Proxy.XSubscriber.XPublisher/Program.cs
+++ Examples/NetMQ-Examples/Demo.05.Proxy.XSubscriber.XPublisher/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NetMQ.Sockets;
+using System;
 
 namespace Demo._05.Proxy.XSubscriber.XPublisher {
     class Program {
+
         static void Main(string[] args) {
+
+            const string defaultProxyEndPointUpstream = @"tcp://localhost:5678";
+            const string defaultProxyEndPointDownstream = @"tcp://localhost:5680";
+
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine($"{args[i]}");
+            }
+
+            string proxyEndPointUpstream = defaultProxyEndPointUpstream;
+            string proxyEndPointDownstream = defaultProxyEndPointDownstream;
+
+            using (var xpubSocket = new XPublisherSocket(proxyEndPointDownstream))
+            using (var xsubSocket = new XSubscriberSocket(proxyEndPointUpstream)) {
+
+                Console.WriteLine($"frontend XSUB bound upstream to {proxyEndPointUpstream}");
+                Console.WriteLine($"backend XPUB bound downstream to {proxyEndPointDownstream}");
+                
+                // create a proxy between frontend and backend
+                var proxy = new NetMQ.Proxy(xsubSocket, xpubSocket);
+                Console.WriteLine($"starting proxy from {proxyEndPointUpstream} to {proxyEndPointDownstream}...");
+
+                // blocks indefinitely
+                proxy.Start();                
+            }
         }
     }
 }
