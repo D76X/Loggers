8582f3b90bee0370bf8f71d33351fef74b8f78d8
Davide Spano <davide.spano.x@gmail.com>
2018-05-27 12:30:43 +0100

    changes to Demo.05 proxy refactoring added teardown code

--- Examples/NetMQ-Examples/Demo.05.NetMQ.MultiplePublishersAndSubscribers/Program.cs
+++ Examples/NetMQ-Examples/Demo.05.NetMQ.MultiplePublishersAndSubscribers/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace Demo._05.NetMQ.MultiplePublishersAndSubscribers {
     class Program {
@@ -9,19 +10,32 @@ namespace Demo._05.NetMQ.MultiplePublishersAndSubscribers {
             // appropropriately.
 
             // First we create all the publishers 
+            const string publisherExePath = @"..\..\..\Demo.05.NetMQ.Publisher\bin\Debug\Demo.05.NetMQ.Publisher.exe";
+            const string subscriberExePath = @"..\..\..\Demo.05.NetMQ.Subscriber\bin\Debug\Demo.05.NetMQ.Subscriber.exe";
+            const string proxyExePath = @"..\..\..\Demo.05.NetMQ.Proxy.XSubscriber.XPublisher\bin\Debug\Demo.05.NetMQ.Proxy.XSubscriber.XPublisher.exe";
+
             Process publisherAll = new Process();
-            publisherAll.StartInfo.FileName = @"..\..\..\Demo.05.NetMQ.Publisher\bin\Debug\Demo.05.NetMQ.Publisher.exe";
-            publisherAll.Start();
+            publisherAll.StartInfo.FileName = publisherExePath;
+            publisherAll.Start();            
 
             // The we create all the subscribers
             Process subscriberAll = new Process();
-            subscriberAll.StartInfo.FileName = @"..\..\..\Demo.05.NetMQ.Subscriber\bin\Debug\Demo.05.NetMQ.Subscriber.exe";
-            subscriberAll.Start();
+            subscriberAll.StartInfo.FileName = subscriberExePath;
+            subscriberAll.Start();           
 
             // Then we start the proxy
             Process proxy = new Process();
-            proxy.StartInfo.FileName = @"..\..\..\Demo.05.NetMQ.Proxy.XSubscriber.XPublisher\bin\Debug\Demo.05.NetMQ.Proxy.XSubscriber.XPublisher.exe";
+            proxy.StartInfo.FileName = proxyExePath;
             proxy.Start();
+
+            Console.WriteLine("press any key to tear down all processes...");
+            Console.ReadKey();
+
+            proxy.CloseMainWindow();
+
+            publisherAll.CloseMainWindow();
+
+            subscriberAll.CloseMainWindow();
         }
     }
 }
