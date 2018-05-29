3348c0a8351dcc094c44a9ee1dcfa16ff228375e
Davide Spano <davide.spano.x@gmail.com>
2018-05-29 13:45:04 +0100

    changes to ZEROMQ demos better names for projects, renamed namespaces, improved tear down code, etc.

--- Examples/ZeroMQ-Examples/Demo.01.ZeroMQ.Starter.REQ.REP/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.01.ZeroMQ.Starter.REQ.REP/Program.cs
@@ -5,39 +5,40 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 
-namespace Demo._02.ZeroMQ.MultipleClients.PUSH.PULL {
-
+namespace Demo._01.ZeroMQ.Starter.REQ {
     class Program {
 
-        static HashSet<Process> processes = new HashSet<Process>();
+        static HashSet<int> pids = new HashSet<int>();
 
         static void StartProcess(string exeRelPath, string arguments) {
 
             Process process = new Process();
-            processes.Add(process);
             process.StartInfo.FileName = Path.GetFullPath(exeRelPath);
             process.StartInfo.Arguments = arguments;
             process.Start();
+            pids.Add(process.Id);
         }
 
         static void Main(string[] args) {
 
-            const string serverExeRelPath = @"..\..\..\Demo.02.ZeroMQ.Server.PULL\bin\Debug\Demo.02.ZeroMQ.Server.PULL.exe";
-            const string clientExeRelPath = @"..\..\..\Demo.02.ZeroMQ.Client.PUSH\bin\Debug\Demo.02.ZeroMQ.Client.PUSH.exe";
+            const string serverExeRelPath = @"..\..\..\Demo.01.ZeroMQ.Server.REP\bin\Debug\Demo.01.ZeroMQ.Server.REP.exe";
+            const string clientExeRelPath = @"..\..\..\Demo.01.ZeroMQ.Client.REQ\bin\Debug\Demo.01.ZeroMQ.Client.REQ.exe";
 
             StartProcess(serverExeRelPath, null);
-            Console.WriteLine("started PULL server");
+            Console.WriteLine("started REP server");
 
             Console.WriteLine("give the server enough time to bind to its end point...");
             Thread.Sleep(1000);
 
             for (int i = 1; i < 5; i++) {
                 StartProcess(clientExeRelPath, null);
-                Console.WriteLine($"started PUSH client {i}");
-            }            
-        
+                Console.WriteLine($"started REQ client {i}");
+            }
+
             Console.WriteLine("press any key to tear down all processes...");
             Console.ReadKey();
+
+            var processes = Process.GetProcesses().Where(p => pids.Contains(p.Id));
             processes.ToList().ForEach(p => p.CloseMainWindow());
         }
     }
