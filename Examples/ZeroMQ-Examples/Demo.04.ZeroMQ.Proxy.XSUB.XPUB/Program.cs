bfe4ad88f6d2ba60916feb5adca4276459c8303c
Davide Spano <davide.spano.x@gmail.com>
2018-05-29 16:22:47 +0100

    changes to implement proxy with XSUB+XPUB+Poller

--- Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Proxy.XSUB.XPUB/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Proxy.XSUB.XPUB/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ZeroMQ;
 
 namespace Demo._04.ZeroMQ.Proxy.XSUB.XPUB {
+
+    /// <summary>
+    /// Refs
+    /// https://github.com/zeromq/clrzmq/blob/master/src/ZeroMQ/Poller.cs
+    /// https://github.com/zeromq/clrzmq/blob/master/src/ZeroMQ/Devices/Device.cs
+    /// https://github.com/zeromq/clrzmq/blob/master/src/ZeroMQ/Devices/ForwarderDevice.cs
+    /// </summary>
     class Program {
+
         static void Main(string[] args) {
 
             // both ends of the device must bind hence @"tcp://*:PORT"
@@ -19,26 +23,58 @@ namespace Demo._04.ZeroMQ.Proxy.XSUB.XPUB {
 
             string proxyEndPointFrontend = defaultProxyEndPointFrontend;
             string proxyEndPointBackend = defaultProxyEndPointBackend;
-            
+
             using (ZmqContext context = ZmqContext.Create())
             using (ZmqSocket xpubSocketBackend = context.CreateSocket(SocketType.XPUB))
-            using (ZmqSocket xsubSocketFrontend = context.CreateSocket(SocketType.XSUB)) {
+            using (ZmqSocket xsubSocketFrontend = context.CreateSocket(SocketType.XSUB))
+            using (Poller poller = new Poller()) {
 
+                // must set up an haldler on the forntend socket to send the message to 
+                // the backend. 
+                xsubSocketFrontend.ReceiveReady += (sender, socketArgs) => {
+
+                    // here something may be done to the message before 
+                    // forwarding to the backend...
+
+                    // forward the message to teh backend
+                    ((ZmqSocket)sender).Forward(xpubSocketBackend);
+
+                };
+
+                xpubSocketBackend.ReceiveReady += (sender, socketArgs) => {
+
+                    // here something may be done to the message before 
+                    // forwarding to the backend... 
+                    //socketArgs.
+
+                    //Frame frame = xpubSocketBackend.ReceiveFrame();
+                    //xpubSocketBackend.Send(frame.Buffer, frame.Buffer.Length,
+                    //    SocketFlags.DontWait);
+                    //Console.WriteLine(frame.Buffer);
+
+                    xpubSocketBackend.SendMessage(xpubSocketBackend.ReceiveMessage());
+                    Console.Write("*");
+                };
+
+                // the poller drives the events between the sockets 
+                poller.AddSockets(new[] { xsubSocketFrontend, xpubSocketBackend });
+
+                // binds or connect the sockets to their endpoint
                 xsubSocketFrontend.Bind(proxyEndPointFrontend);
                 xpubSocketBackend.Bind(proxyEndPointBackend);
 
                 Console.WriteLine($"frontend XSUB bound upstream to {proxyEndPointFrontend}");
                 Console.WriteLine($"backend XPUB bound downstream to {proxyEndPointBackend}");
 
-                // how do I start the proxy?
-                // ....
-                // USE A POLLER ????????
-                // https://github.com/zeromq/clrzmq/blob/master/src/ZeroMQ/Devices/Device.cs
-                // https://github.com/zeromq/clrzmq/blob/master/src/ZeroMQ/Devices/ForwarderDevice.cs
-            }
+                // do the polling every 500 ms
+                // better strategies may be implemented
+                TimeSpan timeout = TimeSpan.FromMilliseconds(500);
 
-            //Console.WriteLine("proxy XSUB-XPUB up and running...");
-            //Console.ReadKey();
+                while (true) {
+                    poller.Poll(timeout);
+                    //Console.WriteLine("forwarding");
+                }
+            }
         }
     }
 }
--- Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Starter.XSUB.XPUB/Program.cs
+++ Examples/ZeroMQ-Examples/Demo.04.ZeroMQ.Starter.XSUB.XPUB/Program.cs
@@ -7,15 +7,15 @@ using System.Linq;
 namespace Demo._04.ZeroMQ.Starter.XSUB.XPUB {
     class Program {
 
-        static HashSet<Process> processes = new HashSet<Process>();
+        static HashSet<int> pids = new HashSet<int>();
 
         static void StartProcess(string exeRelPath, string arguments) {
 
             Process process = new Process();
-            processes.Add(process);
             process.StartInfo.FileName = Path.GetFullPath(exeRelPath);
             process.StartInfo.Arguments = arguments;
             process.Start();
+            pids.Add(process.Id);
         }
 
         static void Main(string[] args) {
@@ -25,12 +25,33 @@ namespace Demo._04.ZeroMQ.Starter.XSUB.XPUB {
             const string publisher2ExePath = @"..\..\..\Demo.04.ZeroMQ.Publisher2.PUB\bin\Debug\Demo.04.ZeroMQ.Publisher2.PUB.exe";
             const string subscriberExePath = @"..\..\..\Demo.04.ZeroMQ.Subscriber.SUB\bin\Debug\Demo.04.ZeroMQ.Subscriber.SUB.exe";
             const string proxyExePath = @"..\..\..\Demo.04.ZeroMQ.Proxy.XSUB.XPUB\bin\Debug\Demo.04.ZeroMQ.Proxy.XSUB.XPUB.exe";
+            const string proxyFrontendEndPointToConnectTo = @"tcp://localhost:5678";
+            const string proxyBackendEndPointToConnectTo = @"tcp://localhost:5680";
 
-            // start the proxy
+            // |tcp://*:6001|READINGS_AREA_1A|8|
+            // start publisher for Area1
+            StartProcess(publisher1ExePath, $"{proxyFrontendEndPointToConnectTo} {proxyFrontendEndPointToConnectTo}");
+            Console.WriteLine($"started publishers for Area1.");
+
+            StartProcess(publisher2ExePath, $"{proxyFrontendEndPointToConnectTo}");
+            Console.WriteLine($"started publishers for Area2.");
+
+            // starts a subscriber to all endpoints from Area1 and all messages from each endpoint.
+            StartProcess(subscriberExePath, $"{proxyBackendEndPointToConnectTo}|READINGS_AREA_1A|8|");
+            Console.WriteLine($"started subscriber to all endpoints for Area1 and all messages to any endpoint.");
+            // starts a subscriber to messages from Area2 with prefix AREA2_TEMPEREATURE.
+            StartProcess(subscriberExePath, $"{proxyBackendEndPointToConnectTo}|AREA2_TEMPEREATURE|1|");
+            Console.WriteLine($"started subscriber to messages with prefix AREA2_TEMPEREATURE from Area2 endpoint.");
+
+            // Thanks to ZEROMQ sockets publishers and the subscribers will automatically connect to the 
+            // corresponding end points of the proxy even when the proxy is started at a later 
+            // point in time.
             StartProcess(proxyExePath, null);
 
             Console.WriteLine("press any key to tear down all processes...");
             Console.ReadKey();
+
+            var processes = Process.GetProcesses().Where(p => pids.Contains(p.Id));
             processes.ToList().ForEach(p => p.CloseMainWindow());
         }
     }
