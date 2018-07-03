7273130810cd0c50e9ad1d204033f3c69ba30ae6
Davide Spano <davide.spano.x@gmail.com>
2018-07-03 15:18:30 +0100

    Example IoTHub message processor working

--- Examples/IoT-Hub-Examples/Test.Band.MessageProcessor1/LoggingEventProcessor.cs
+++ Examples/IoT-Hub-Examples/Test.Band.MessageProcessor1/LoggingEventProcessor.cs
@@ -10,45 +10,87 @@ namespace Test.Band.MessageProcessor1 {
 
     class LoggingEventProcessor : IEventProcessor {
 
+        /// <summary>
+        /// Called when the processor is attached to a new partition for a Hub.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public Task OpenAsync(PartitionContext context) {
             Console.WriteLine("LoggingEventProcessor opened, processing partition: " +
                               $"'{context.PartitionId}'");
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Called when the processor is detached from a partition for a Hub.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="reason"></param>
+        /// <returns></returns>
         public Task CloseAsync(PartitionContext context, CloseReason reason) {
             Console.WriteLine("LoggingEventProcessor closing, partition: " +
                               $"'{context.PartitionId}', reason: '{reason}'.");
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Called any time something goes wrong with the processor.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
         public Task ProcessErrorAsync(PartitionContext context, Exception error) {
             Console.WriteLine("LoggingEventProcessor error, partition: " +
                               $"{context.PartitionId}, error: {error.Message}");
             return Task.CompletedTask;
         }
 
-        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages) {
+        /// <summary>
+        /// As data reaches the IoT Hub it will be passed to this method for 
+        /// processing.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="messages">a batch of messages from the Hub</param>
+        /// <returns></returns>
+        public Task ProcessEventsAsync(
+            PartitionContext context, 
+            IEnumerable<EventData> messages) {
+
             Console.WriteLine($"Batch of events received on partition '{context.PartitionId}'.");
 
+            // go through the batch of messages to process
             foreach (var eventData in messages) {
+
+                // extract teh payload that was sent by the the device up to the 
+                // hub as part of the message. Remember that the device can push 
+                // any serialized object up to the IoT Hub!
                 var payload = Encoding.ASCII.GetString(eventData.Body.Array,
                     eventData.Body.Offset,
                     eventData.Body.Count);
 
+                // the event as other information attached to it that can 
+                // be also used by the processor logic. Some info mayh be 
+                // about the Hub and other baout the device that sent the 
+                // message to it.
                 var deviceId = eventData.SystemProperties["iothub-connection-device-id"];
 
                 Console.WriteLine($"Message received on partition '{context.PartitionId}', " +
                                   $"device ID: '{deviceId}', " +
                                   $"payload: '{payload}'");
 
-                var telemetry = JsonConvert.DeserializeObject<Telemetry>(payload);
+                //var telemetry = JsonConvert.DeserializeObject<Telemetry>(payload);
 
-                if (telemetry.Status == StatusType.Emergency) {
-                    Console.WriteLine($"Guest requires emergency assistance! Device ID: {deviceId}");
-                    SendFirstRespondersTo(telemetry.Latitude, telemetry.Longitude);
-                }
+                //if (telemetry.Status == StatusType.Emergency) {
+                //    Console.WriteLine($"Guest requires emergency assistance! Device ID: {deviceId}");
+                //    SendFirstRespondersTo(telemetry.Latitude, telemetry.Longitude);
+                //}
             }
+
+            // this call is important because it causes the processor to write a 
+            // checkpoint to the checkpoint blob storage. The processor uses this 
+            // blob storage to keept track of the messages on th eIoT Hub that it
+            // has already processed. Without this call the processor would reprocess
+            // all the messages on the IoT Hub each time it runs.
             return context.CheckpointAsync();
         }
 
